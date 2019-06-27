4d6e8d9aec4dcec7fa5d04e20dea957e8ec878c7
littleflute <littleflute@163.com>
2019-06-26 21:12:26 -0700

    +issues/9/*.png

--- gdx/test/core/src/com/mygdx/game/MyGdxGame.java
+++ gdx/test/core/src/com/mygdx/game/MyGdxGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+// Run -> Edit configuartions...
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
