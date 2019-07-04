85a97d25afc768e1c1893097baa09a6be7e1a288
littleflute <13699175164@sina.cn>
2019-07-04 15:04:16 -0700

    c2.js v0.0.22

--- issues/4/c2.js
+++ issues/4/c2.js
@@ -1,4 +1,4 @@
-var s = "id_mdiv_4_i4c2: v0.0. 13 - ";
+var s = "id_mdiv_4_i4c2: v0.0. 23 - ";
 s += "<a target='_blank' href='https://github.com/jeremyjia/Games/edit/master/issues/4/c2.js'"
 s += " style='color:blue;'";	s +=">"; s += "c2.js* ";
 s += "<a target='_blank' href='https://jeremyjia.github.io/Games/issues/4/c2.js'"
@@ -8,6 +8,9 @@ s += " style='color:green;'";	s +=">"; s += "c2Test.html";
 
 var d = blo0.blMDiv(document.body,"id_mdiv_4_i4c2", s,    700,100,500,400, blGrey[5]); 
 if(!d.v1){
+    d. vToolBar = blo0.blDiv(d, d.id + "vToolBar ", 'vToolBar :', blGrey[1]);
+    d. vToolBar. btn1 = blo0.blBtn(d. vToolBar, d. vToolBar.id + "btn1 ", ' btn1', blGrey[1]);
+    d. vToolBar. btn1 .onclick = function(){alert("click me, to create a new map.")}
     d.v1 = blo0.blDiv(d, d.id + "v1", '<canvas id="canvas1" width="400" height="400"></canvas>', blGrey[1]);
     d.v1.style.width = "500px";
     d.v1.style.height = "400px";
