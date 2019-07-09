3fb901c78c664265a08f8a27e188ec8bee1e9e01
littleflute <littleflute@163.com>
2019-07-08 17:35:34 -0700

    c1.js v0.0.42

--- issues/4/c1.js
+++ issues/4/c1.js
@@ -1,5 +1,5 @@
 //i4c1
-var s= "v0.0. 33 ";
+var s= "v0.0. 42 ";
 s += "<a target='_blank' href='https://github.com/jeremyjia/Games/edit/master/issues/4/c1.js'"
 s += " style='color:blue;'";		s +=">"; s += "c1.js* ";
 s += "<a target='_blank' href='https://jeremyjia.github.io/Games/issues/4/c1.js'"
@@ -36,8 +36,17 @@ if(!md.run){
 }
 _on_off_div(this,md);
 
-function ftnPlayer(oDiv){
+function ftnPlayer( oDiv ){
 	var v = blo0.blDiv(oDiv, oDiv.id + "Player", "Player",blGrey[0]);
+	var str4V = '<video id="myVideo" width="320" height="240" controls> ';				
+	str4V += '<source src=';
+	str4V += 'https://littleflute.github.io/ted1/docs/61/v0.mp4';
+	str4V +=' type="video/mp4">Your browser does not support HTML5 video.'; 	
+	str4V += '</video>';  										
+	var vPlayer = blo0.blDiv( v ,"id_div_4_myVideo" , str4V , 300,100,500,400,blColor[1]); 	
+	var vid = document.getElementById("myVideo"); 
+	vid.controls = false;
+
 	v.tb = blo0.blDiv(v, v.id + "tb", "tb",blGrey[3]);
 	v.tb.b1 = blo0.blBtn(v.tb, v.tb+"b1","b1",blGrey[0]);
 	v.tb.b1.onclick = function(){
@@ -51,11 +60,12 @@ function ftnPlayer(oDiv){
 		if(!this.run){
 			this.run = true;
 			this.style.backgroundColor = "green";
+			vid.play();
 		} 
 		else{
 			this.run = false;
 			this.style.backgroundColor = "brown";
+			vid.pause();
 		}
 	}
-
 }
