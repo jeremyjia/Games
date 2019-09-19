c5a061082e65e8436e98d6f9f2b0be157553567c
littleflute <littleflute@163.com>
2019-09-19 15:15:47 -0700

    +2,3,4.js

--- node/1.js
+++ node/1.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var dt 		= require('./module1');
+var url		= require('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("dt:" + dt.myDateTime() + "<br>");
+  res.write("url:" + req.url + "<br>");
+  var q = url.parse(req.url, true).query;
+  res.write(q.a + "<br>");
   res.end('Hello World! by xd.');
 }).listen(8080);
