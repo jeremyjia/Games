154cdda108e9af0d62863cc2be6c9baa9994fddf
littleflute <littleflute@163.com>
2019-09-19 15:36:15 -0700

    7.js*

--- node/7.js
+++ node/7.js
@@ -1,13 +1,19 @@
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      res.write('File uploaded');
-      res.end();
-    });
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/User/github/Games/node/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
