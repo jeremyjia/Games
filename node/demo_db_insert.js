6830adac155fe3ee277c23bd2bd77906b446fb33
littleflute <littleflute@163.com>
2019-09-26 23:54:47 -0700

    +demo_db_insert.js

---
+++ node/demo_db_insert.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "xd178090xd",
+  database: "mydb"
+});
+ 
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
