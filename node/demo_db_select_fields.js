ce320c756d6bc47b493e6bca974397196f5583f4
littleflute <littleflute@163.com>
2019-10-03 18:30:42 -0700

    +demo_da_fields.js

--- node/demo_db_select_fields.js
+++ node/demo_db_select_fields.js
@@ -9,8 +9,8 @@ var con = mysql.createConnection({
  
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT name, address FROM customers", function (err, result, fields) {
+  con.query("SELECT * FROM customers", function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
+    console.log(fields);
   });
 });
