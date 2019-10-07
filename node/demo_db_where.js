f312c08c2de6f2c6b3e71bcdf89e0b74301aebfd
littleflute <littleflute@163.com>
2019-10-06 23:40:43 -0700

    +demo_db_where.js

---
+++ node/demo_db_where.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "xd178090xd",
+  database: "mydb"
+});
+ 
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
