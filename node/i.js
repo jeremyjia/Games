aa48bb32b3c1c4447e7f1eeff97daeaed29a0c77
littleflute <littleflute@163.com>
2019-10-23 02:11:00 -0700

    delete done

--- node/i.js
+++ node/i.js
@@ -4,8 +4,36 @@ var fs = require('fs');
 var mysql = require('mysql');
 
 http.createServer(function (req, res) {
-  if (req.url == '/delete') {
-      res.write('to do: delete...');  
+  console.log(req.url);
+  var xdURL = req.url;
+  var a = xdURL.split("?");
+  var b = a[1];
+  console.log(a[0]);
+
+  if (a[0] == '/delete') { 
+      var xdAddr = "Lowstreet 4";
+      var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "xd178090xd",
+        database: "mydb"
+      });
+     
+      con.connect(function(err) {
+        if (err) throw err;
+        //var sql = "DELETE FROM customers1 WHERE address = 'Mountain 21'";
+        var sql = "DELETE FROM customers1 WHERE " + b;
+        var sql1 = sql.replace(/%27/g,"'");
+        var sql2 = sql1.replace(/%20/g," "); 
+        console.log(sql2);
+        con.query(sql2, function (err, result) {
+          if (err) throw err;
+          console.log("Number of records deleted: " + result.affectedRows);
+        });
+      });
+
+      res.write('to do: delete...'); 
+
       res.end();
   }
   else if (req.url == '/show') {
