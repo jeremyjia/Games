0c79dfb571415377d9add711344e85df30f799b1
littleflute <littleflute@163.com>
2019-10-31 00:31:27 -0700

    *index.html

--- node/iInsert.js
+++ node/iInsert.js
@@ -1,6 +1,6 @@
 
 var mysql = require('mysql');
-
+var nAdd = 0;
 exports.AddRow = function (res,tb) {
 	console.log("insert 1:" + tb); 
   res.write('<br> to AddRow:  ...' + tb); 
@@ -14,7 +14,11 @@ exports.AddRow = function (res,tb) {
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "INSERT INTO customers1 (name, address) VALUES ('insert test', 'a test 1')";
+    var d = new Date();
+    nAdd++;
+    var sql = "INSERT INTO customers1 (name, address) VALUES ('";
+    sql += "Name " +nAdd;
+    sql += "', 'A: "+d+" [v0.11]')";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
