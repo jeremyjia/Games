599e408f704ddc48a7a97cf44a32d96a6b4a91d3
Jeremy Jia <jjia@jjia-macbookpro.opentext.net>
2019-11-13 21:22:02 +0800

    submit tool

---
+++ pbzTools/pbzTools/src/com/NetUtil.java
@@ -0,0 +1,104 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class NetUtil {
+
+	/**
+	 * Specify the URL to send GET request
+	 * 
+	 * @param url   request URL
+	 * @param param format should be name1=value1&name2=value2
+	 * @return URL response string
+	 */
+	public static String sendGetRequest(String url, String param) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			URLConnection connection = realUrl.openConnection();
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.connect();
+			Map<String, List<String>> map = connection.getHeaderFields();
+			for (String key : map.keySet()) {
+				// System.out.println(key + "--->" + map.get(key));
+			}
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("Error when sending GET request！" + e);
+			e.printStackTrace();
+		} finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Specify the URL to send POST request.
+	 *
+	 * @param url   request URL
+	 * @param param format should be name1=value1&name2=value2
+	 * @return response
+	 */
+	public static String sendPostRequest(String url, String param) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(url);
+			URLConnection conn = realUrl.openConnection();
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			out = new PrintWriter(conn.getOutputStream());
+			out.print(param);
+			out.flush();
+			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("Error when sending POST request！" + e);
+			e.printStackTrace();
+		} finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static String getToken() {
+		return "f89b0eccf7" + "4c65a65513" + "60062c3e47" + "98d0df4577"; // Jeremyjia's
+	}
+}
