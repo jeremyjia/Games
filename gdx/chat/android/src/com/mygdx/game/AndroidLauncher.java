062f8e35075d7107b834f92ceb6060e9175a0964
Jeremy Jia <jjia@jjia-macbookpro.opentext.net>
2020-01-09 16:34:05 +0800

    fivechess

--- gdx/chat/android/src/com/mygdx/game/AndroidLauncher.java
+++ gdx/chat/android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.GdxChatGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new GdxChatGame(), config);
+		initialize(new GdxGameAdapter(), config);
 	}
 }
--- gdx/chat/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ gdx/chat/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.GdxChatGame;
+import com.mygdx.game.GdxGameAdapter;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GdxChatGame(), config);
+		new LwjglApplication(new GdxGameAdapter(), config);
 	}
 }
