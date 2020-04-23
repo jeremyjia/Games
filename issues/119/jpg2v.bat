2038b3f22df89a2e61954ff64ca7c8d4ee66c65e
littleflute <littleflute@163.com>
2020-04-23 10:36:35 -0700

    jpg2v.bat*

--- issues/119/jpg2v.bat
+++ issues/119/jpg2v.bat
@@ -1,1 +1,9 @@
-ffmpeg -i %%d.jpg -bf 0 -g 1 -s 240x240 -vcodec mpeg4 v60s.mp4  
+echo jpg2v.bat V0.0. 13 ;
+
+
+ffmpeg -framerate 1 -i %%d.jpg -r 1 -s 240x240 -y v60s.mp4  
+
+echo ffmpeg -framerate 10 -i Path/To/File/filename%3d.jpg -r 5 -y Path/To/File/test.mp4
+
+
+echo debug:  ...
