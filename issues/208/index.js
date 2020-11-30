ffeba10f22b5d925284810dd654e66bc9fb39b4a
unknown <yongling.huang@group6.io>
2020-11-30 02:51:51 -0800

    exeshell.js*

--- issues/208/index.js
+++ issues/208/index.js
@@ -1,7 +1,9 @@
-const tag = "[index.js_v0.0.3]";
+const tag = "[index.js_v0.0.11]";
 
+const e = require('express');
 const express = require('express')
 var request = require('request');
+var es = require('./exeshell.js');
 
 const app = express()
 const port = 3000
@@ -9,6 +11,17 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send(tag + ' Hello World!')
 })
+
+app.get('/exeshell', (req, res) => {
+	console.log(req.url);
+	var a = req.url;
+	var b = a.split('?');
+	var c = b[1].split('cmd=');
+	var scmd = c[1];
+	var s = es.run(res,scmd);
+	//res.send(tag + ' s=' +s);
+})
+
 app.get('/geturl', function(req, res){
     var url = "https://learningenglish.voanews.com/a/study-suggests-people-who-got-covid-19-protected-for-months/5670611.html";
 	console.log(url);
