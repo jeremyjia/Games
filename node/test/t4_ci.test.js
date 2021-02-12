dd91cabcfdef410edad9f77db9625e6f54aaa734
unknown <yongling.huang@group6.io>
2021-02-12 00:35:50 -0800

    t4_ci.test.js*

--- node/test/t4_ci.test.js
+++ node/test/t4_ci.test.js
@@ -1,4 +1,4 @@
-const tag = "[t3.test.js_v0.25]";
+const tag = "[t4_ci.test.js _v0.25] ";
 const config = require('../config'); 
 const ExpressServer = require('../expressServer');
 const ES = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
@@ -6,14 +6,12 @@ const request = require('supertest');
 var assert = require('chai').assert; 
 var testData = require('../auth/data/testData.js');
 var ds = testData.getUserDatas(10); 
-var ts = "auto test 3:";
-ts += "login & getAllPlayers & getPendingFriends & getFriends";
-ts += " & RequestToMakeFriend";
-ts += " & ReponseToMakeFriend";
+var ts = "auto test 4:"; 
+
 describe(ts, function() { 
     var token = "";
     var n = 1; 
-    it('Test 3.'+n+': login.', function() {     
+    it(tag+n+': login.', function() {     
           return request(ES.app)
             .post('/api/login')
             .send(ds[1])
@@ -32,7 +30,7 @@ describe(ts, function() {
 
     n++;
     var resAllPlayers = null;
-    it('Test 3.'+n+': getAllPlayers.', function() {    
+    it(tag +n+': getAllPlayers.', function() {    
       return request(ES.app)
         .get('/api/getAllPlayers')
         .send(ds[1])
@@ -53,84 +51,8 @@ describe(ts, function() {
             assert(n==i,"n=" + n);
             n++;
           }   
-          assert(n==10,"n=" + n);
+          assert(n==11,"n=" + n);
       }) 
     });     
-     
- 
-
-    n++;
-    var allPlayers = null;
-    it('Test 3.'+n+': RequestToMakeFriend.', function() {     
-          var o = {};
-          o.FromID  = testData.getUserID("u1",allPlayers); 
-          o.ToID    = testData.getUserID("u2",allPlayers);
-          o.status  = "Unkown";
-          return request(ES.app)
-            .post('/api/RequestToMakeFriend')
-            .send(o)
-            .set('accept', 'application/json')
-            .set('Content-Type','application/json') 
-            .set('Authorization','Bearer ' + token) 
-            .expect(200) 
-            .then(response => {
-                  console.log(tag + "****************** respoinse.body=", response.body); 
-                  assert(response.body.code == 1,"RequestToMakeFriend error!");
-          }) 
-    });   
-
-    n++; 
-    var allPendingFriends = null;
-    it('Test 3.'+n+': getPendingFriends.', function() {    
-      return request(ES.app)
-        .get('/api/getPendingFriends')
-        .send(ds[1])
-        .set('accept', 'application/json')
-        .set('Authorization','Bearer ' + token) 
-        .expect(200) 
-        .then(response => {  
-          //var s = "allPendingFriends= "+response.body.str;          eval(s);
-          allPendingFriends = response.body.str;
-          assert(response.body.code == 1, "allPendingFriends.RequestID = " + allPendingFriends.RequestID)         
-           
-      }) 
-    }); 
-
-   //*
-    n++;
-    it('Test 3.'+n+': ReponseToMakeFriend.', function() {     
-          var o = {};
-          o.FromID  = testData.getUserID("u1",allPlayers);  
-          o.ToID    = testData.getUserID("u2",allPlayers);  
-          o.ReqID   = testData.getReqID(o.FromID,o.ToID,allPendingFriends);
-          o.status  = "Yes";
-          return request(ES.app)
-            .post('/api/ReponseToMakeFriend')
-            .send(o)
-            .set('accept', 'application/json')
-            .set('Content-Type','application/json') 
-            .set('Authorization','Bearer ' + token) 
-            .expect(200) 
-            .then(response => {
-                  console.log(tag + "****************** respoinse.body=", response.body); 
-          }) 
-    });   
-    //*/
-        n++;
-	it('Test 3.'+n+': getFriends.', function() {
-            return request(ES.app)
-             .get('/api/getFriends')
-             .send(ds[1])
-             .set('accept', 'application/json')
-             .set('Authorization','Bearer ' + token)
-             .expect(200)
-             .then(response => {
-                  console.log(tag + "****************** token=", token);
-                  console.log(tag + "****************** response.body=", response.body);
-                  assert(response.body.code == 1, "response.body.code="+response.body.code);
-                  var s = "var ls="+response.body.str;
-                  eval(s);
-                  assert(ls[0].fromID == testData.getUserID("u1",allPlayers),"fromID="+ls[0].fromID);
-            })
-        });	
+       
 });
