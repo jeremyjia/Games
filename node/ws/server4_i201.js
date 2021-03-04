8c34e02c84a8d0113ee86fa7295821035b31e703
unknown <yongling.huang@group6.io>
2021-03-03 23:57:49 -0800

    ***

--- node/ws/server4_i201.js
+++ node/ws/server4_i201.js
@@ -1,10 +1,11 @@
-const tag = "[ws/server4_i201.js_v0.42] ";
+const tag = "[ws/server4_i201.js_v0.43] ";
 const l = require('../logger');
 l.tag(tag); 
 const eo = {};
 var suits = ["spades", "hearts", "clubs", "diams"];
 var cardFace = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 var cards = [];
+
 function buildCards() {
     cards = [];
     for (s in suits) {
@@ -19,12 +20,22 @@ function buildCards() {
         cards.push(card);
       }
     } 
-  }
+}
 
+function shuffleArray(array) {
+    for (var x = array.length - 1; x > 0; x--) {
+      var ii = Math.floor(Math.random() * (x + 1));
+      var temp = array[x];
+      array[x] = array[ii];
+      array[ii] = temp;
+    }
+    return array;
+  }
 eo.toDo = function(result,clientList){   
     if (result.method === "M_i_201") {  
         
         buildCards();
+        shuffleArray(cards);
 
         const payLoad = {
             "method"        : "M_i_201", 
