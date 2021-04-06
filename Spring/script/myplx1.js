7a4eaa7d2ea7d976610f98f360a53047fb09c974
unknown <yongling.huang@group6.io>
2021-04-06 12:42:29 -0700

    myplx1.js*

--- Spring/script/myplx1.js
+++ Spring/script/myplx1.js
@@ -1,17 +1,31 @@
+
+var nTimes = 0;
 function animateFrame(time) {
+    if(time%100==0){
+        nTimes++;
+        if(nTimes>8) nTimes = 1;
+    }
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
             
     ctx.clearRect(0, 0, 2048, 1536);
 
     ctx.fillStyle = "red";
-    var x = time;
-    ctx.fillRect(10, 10, 100, 100);
+    for(var n = 0; n < nTimes; n++){
+        ctx.fillRect(10 + n*110, 10, 100, 100); 
+        ctx.font = "30px Arial";
+        ctx.fillText(n+1, 10 + n*110, 210); 
+    }
 
+    
+    var x = time;
     ctx.fillStyle = "blue";
-    for (var i = 0; i < 555; i++) {
-        var x = i;
-        var y = 222;
+    for (var i = 0; i < 110; i++) {
+        var x = 10+i; 
+        var y = 122;
+        if(nTimes%2) y = 122 + i;
+        else y = 244 - i;
+        x += 110*(nTimes-1);
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, Math.PI * 2, true);
         ctx.fill();
