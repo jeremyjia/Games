8996bac98c49f0db6c01460ce932cee7caca02fe
jeremyjia <919846792@qq.com>
2021-04-11 15:12:54 +0800

    Update app.js

--- node1/app.js
+++ node1/app.js
@@ -1,11 +1,6 @@
-
 const sq = require('./sequelize')
 
 module.exports = function(){
   var s = sq.f1();
-<<<<<<< HEAD
-  return 'to test plugin.js: to do: ctx.fillText ...';
-=======
   return 'to test plugin.js: ok!';
->>>>>>> 345a920b06be8f79627b0122342d28335c4ba048
 }
