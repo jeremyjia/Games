04b213174dd337eb9f0e3b937cfdbb9b11c45484
unknown <yongling.huang@group6.io>
2021-04-25 17:16:58 -0700

    asItIs.js+

---
+++ js/plx/voa/asItIs.js
@@ -0,0 +1,5 @@
+﻿const tag = "js\plx\voa\asItIs.js_v0.13"; 
+var po = bl$("id_tb4VOA"); po.innerHTML = "voa";
+var ui = blo0.blDiv(po,po.id+"ui","uiVOA",blGrey[1]);
+ui.style.float="left";
+var b1 = blo0.blBtn(ui,ui.id+"b1","b1",blGrey[0]); 
