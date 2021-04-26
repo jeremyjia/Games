22f8b8fe4782427fb96ef5ddee1945a95a0ea7b1
unknown <yongling.huang@group6.io>
2021-04-25 22:44:32 -0700

    test/appTest.js*

--- node1/test/appTest.js
+++ node1/test/appTest.js
@@ -3,11 +3,6 @@ const app = require('../app');
 
 describe('App', function(){
   it('test1', function(){
-<<<<<<< HEAD
-    assert.equal(app(),'to test i297: done.');
-  }); 
-=======
     assert.equal(app(),'to test PGN: ok!');
   });
->>>>>>> master
 });
