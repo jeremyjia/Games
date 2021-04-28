3036ee467e8c9f23c331ef5bb9782b265bf07b40
unknown <yongling.huang@group6.io>
2021-04-27 22:42:47 -0700

    app4i264Test.js*

--- node1/test/app4i264Test.js
+++ node1/test/app4i264Test.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const app = require('../app4i264.js');
 
-describe('App', function(){
+describe('App4i264Test', function(){
   it('test1', function(){
     assert.equal(app(),'to test i264: done.');
   }); 
