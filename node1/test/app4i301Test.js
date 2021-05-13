62d4502c6481e22e48925b8a081e8c0d9045b948
littleflute <littleflute@163.com>
2021-05-13 13:17:15 -0700

    test*

--- node1/test/app4i301Test.js
+++ node1/test/app4i301Test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const app = require('../app4i301');
 
 describe('App', function(){
-  it('test1', function(){
+  it('test: app4i301', function(){
     assert.equal(app(),'to test app4i301: done');
   });
 });
