dff77542b4682ae0764a7829c215ffecd7a83c93
littleflute <littleflute@163.com>
2021-05-13 16:37:02 -0700

    myplx1.js*

--- Spring/script/myplx1.js
+++ Spring/script/myplx1.js
@@ -1,21 +1,44 @@
-var nTimes = 0;
+var strV = "[myplx1.js] _ v0.24";
+var nIndex = 0;
+
+function CFrame(){
+    var _n = 0;
+    var _fpp = function(_i){
+        if(_i%100==0){
+            _n++;
+        }
+    }
+    var _render = function(ctx,_i){
+        _fpp(_i);
+        ctx.fillText(_n, 333 , 22); 
+    }
+    this.frame = function(ctx,_i){
+        _render(ctx,_i);
+        ctx.fillText(strV, 10 , 22); 
+    };
+};
+
+var f = new CFrame();
+
 function animateFrame(time) {
     if(time%100==0){
-        nTimes++;
-        if(nTimes>8) nTimes = 1;
+        nIndex++;
+        if(nIndex>8) nIndex = 1;
     }
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');      
     ctx.clearRect(0, 0, 2048, 1536);
 
+    f.frame (ctx,time);    
+
     ctx.fillStyle = "red";
-    for(var n = 0; n < nTimes -1 ; n++){
-        ctx.fillRect(10 + n*110, 10, 100, 100); 
+    for(var n = -1; n < nIndex -1 ; n++){
+        ctx.fillRect(10 + n*110, 33, 100, 100); 
         ctx.font = "30px Arial";
         ctx.fillText(n+1, 10 + n*110, 210); 
     }
 
-    var nn = Math.floor(nTimes);
+    var nn = Math.floor(nIndex);
     ctx.font = "30px Arial";
     ctx.fillText(nn, 10 , 310); 
 
@@ -27,7 +50,7 @@ function animateFrame(time) {
         x = 10+i;  
         if(nn%2) y = 122 + i;
         else y = 244 - i;
-        x += 110*(nTimes-1);
+        x += 110*(nIndex-1);
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, Math.PI * 2, true);
         ctx.fill();
