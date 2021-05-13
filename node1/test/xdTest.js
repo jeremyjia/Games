582bb5548d4dce2d32ddbc271162de75ed290e47
littleflute <littleflute@163.com>
2021-05-13 13:09:40 -0700

    xd.js

---
+++ node1/test/xdTest.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const app = require('../app/xd.js');
+
+describe('App', function(){
+  it('xdTest', function(){
+    assert.equal(app(),'to test xdApp: done');
+  });
+});
