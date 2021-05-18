5ef3760320e4435953cc0240345a7d58e248ed93
littleflute <littleflute@163.com>
2021-05-17 20:06:15 -0700

    test.py*

--- python/test.py
+++ python/test.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import subprocess
 
 app = Flask("__main__")
 
 @app.route("/")
 def home():
-    return "hello"
+    p1 = subprocess.run('ls','-la'],capture_output=True)
+    return p1.stdout
 
 if __name__ == "__main__":
     app.run()
