ce62a0f951799ec692b72a416cd5c78a8347b6bc
littleflute <littleflute@163.com>
2021-05-17 20:30:06 -0700

    test.py*

--- python/test.py
+++ python/test.py
@@ -1,10 +1,9 @@
-from flask import Flask 
-
-app = Flask("__main__")
+from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
-def home(): 
-    return "xd"
+def hello():
+    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host="localhost", port=8080, debug=True)
