eca6c20f3e6af4d4e25841232931c2e5fca640c2
littleflute <littleflute@163.com>
2021-05-20 17:06:27 -0700

    littlefluteForkTest.js

--- node1/test/littlefluteForkTest.js
+++ node1/test/littlefluteForkTest.js
@@ -1,8 +1,8 @@
 const assert = require('chai').assert;
 const app = require('../app/littlefluteFork.js');
 
-describe('xdApp', function(){
-  it('xdTest', function(){
+describe('littlefluteFork', function(){
+  it('littlefluteFork', function(){
     assert.equal(app(),'to test littlefluteForkApp(fix #352): done.');
   });
 });
