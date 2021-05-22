3ce4f5eb5b49fd84593e76ad803cbb19af581793
unknown <yongling.huang@group6.io>
2021-05-22 11:36:36 -0700

    sendGrid.js*

--- node1/app/sendGrid.js
+++ node1/app/sendGrid.js
@@ -2,7 +2,7 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
-
+// set pbzEmailKey=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
   
 const sgMail = require('@sendgrid/mail')
 
@@ -10,24 +10,24 @@ var e = {};
 
 module.exports = e;
 
-e.sendMail = function(){
+e.sendEmail  = function(){
+    sgMail.setApiKey(process.env.pbzEmailKey)
+    const msg = {
+    to: 'yongling@group6.io', // Change to your recipient
+    from: '13699175164@sina.cn', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
 
     return "test to send email";
 }
 
-sgMail.setApiKey(process.env.pbzEmailKey)
-const msg = {
-  to: 'yongling@group6.io', // Change to your recipient
-  from: '13699175164@sina.cn', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
