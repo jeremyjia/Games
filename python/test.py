6f1edd088071cee01bad737764094e84b94b9789
unknown <yongling.huang@group6.io>
2021-05-29 11:20:12 -0700

    test.py

--- python/test.py
+++ python/test.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+    
+@app.route("/api/NewPlayer")
+def NewPlayer():
+    return "Hello NewPlayer!"
+
+     
+@app.route("/api/checkUserName")
+def checkUserName():
+    return "Hello checkUserName!"
+
+     
+
+
 if __name__ == '__main__':
     app.run(host="localhost", port=8080, debug=True)
+
+    
+ 
