c25d3562e2b1065294ab3442e243ca3b95254584
unknown <yongling.huang@group6.io>
2021-05-29 21:47:55 -0700

    plxChessBaord.js

--- pbzTools/scriptengine/plugin/plxChessBoard.js
+++ pbzTools/scriptengine/plugin/plxChessBoard.js
@@ -1,13 +1,17 @@
 // https://www.jb51.net/article/105958.htm
 
 function animateFrame(time) {
-	obj.init();
+	obj.XdInit();
 	obj.init_chess();
 }
+function Class1(){
 
-var obj = {};
+}
+var obj = new Class1();
+
+//var obj = {};
 // 初始化
-obj.init = function (args) {
+obj.XdInit = function () {
 	var canvas = document.getElementById("myCanvas");
 	this.ctx = canvas.getContext("2d");
 	this.radius = 23;
@@ -28,7 +32,7 @@ obj.init_back = function () {
 }
 // 棋子初始化
 obj.init_chess = function () {
-	var Car_b1 = { x: 1, y: 1, text: "車" }
+	var Car_b1 = { x: 1, y: 1, text: "2-車" }
 	var Horse_b1 = { x: 2, y: 1, text: "馬" }
 	var Elephant_b1 = { x: 3, y: 1, text: "象" }
 	var Scholar_b1 = { x: 4, y: 1, text: "士" }
