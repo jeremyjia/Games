c5dfe0e1826243ffe73e2ab27bc7a99763c119d7
unknown <yongling.huang@group6.io>
2021-06-11 21:50:29 -0700

    app.py*

--- python/swagger/app.py
+++ python/swagger/app.py
@@ -42,10 +42,7 @@ APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 
 APP.register_blueprint(request_api.get_blueprint())
-
-@APP.route('/')
-def index():
-    return "This is an python-swaggerUI v0.11"
+ 
 
 @APP.errorhandler(400)
 def handle_400_error(_error):
