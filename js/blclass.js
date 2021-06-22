3850c61c9b90385cbba3501e42f59eb8756c4033
unknown <yongling.huang@group6.io>
2021-06-22 04:04:46 -0700

    *

--- js/blclass.js
+++ js/blclass.js
@@ -1,5 +1,5 @@
 // file: blclass.js    by littleflute 
-var g_ver_blClass = "CBlClass_v1.4.233"
+var g_ver_blClass = "CBlClass_v1.4.234"
 function myAjaxCmd(method, url, data, callback){
 	var xmlHttpReg = null;
 	if (window.XMLHttpRequest){
@@ -289,8 +289,8 @@ function CBlClass ()
 		this.blrAddFrames = function(b,d){
 			for(var i = 0; i < _blVideo.duration; i++){
 				var n = _frames.length;
-				var B = 11;//n*50%255;
-				var f = new CFrame(n,"1","111,2," + B);
+				var B = 122;//n*50%255;
+				var f = new CFrame(n,"1","111,222," + B);
 				var t1 = {
 					"text": i + ": by Littleflute", 
 					"x": 100,
