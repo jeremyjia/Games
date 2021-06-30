6cbf84ec4d89e8a9e6b6351d217c47aee143521d
littleflute <littleflute@163.com>
2021-06-30 14:56:23 -0700

    _i475

--- issues/4/c3/sample1.js
+++ issues/4/c3/sample1.js
@@ -6,7 +6,7 @@ _btn.click();
 
 
 function CSample1 (_id){
-    var _v = "CSample1_v0.55 (auto test framework)";
+    var _v = "CSample1_v0.111 (auto test framework)";
     this.getValue = function(){
         var s = "// ";
         s += _v;
@@ -18,9 +18,9 @@ function CSample1 (_id){
         s += _addFun2Obj("os1","bll2","'-2-'");
         s += _addFun2Obj("os1","blrTest_download",blrTest_download);
         s += _addFun2Obj("os1","bll3","'-3-'");
-        s += "var run="+_f1;
+        s += "var toRun="+_fn4_run;
         s += "\n";
-        s += "run(os1,'" + _v + "');";
+        s += "toRun(os1,'" + _v + "');";
         return s;
     }
 
@@ -81,7 +81,7 @@ function CSample1 (_id){
             _on_off_div(b,d);
         }
     }
-    var _f1 = function (_o2show,_uiName){
+    var _fn4_run = function (_o2show,_uiName){
         var d = blo0.blMD(_uiName, _uiName,555,100,500,400, "lightgreen"); 
         d.v = blo0.blDiv(d,d.id+"v","v",blGrey[0]);
         blo0.blShowObj2Div(d.v,_o2show);        
