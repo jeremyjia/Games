a89abd58ed0decd13c68e409022a6baf71b3f79d
jeremy <jeremy@jeremydeMacBook-Pro.local>
2021-08-09 21:26:50 +0800

    upload file to github

--- node1/test/python.js
+++ node1/test/python.js
@@ -3,6 +3,6 @@ const app = require('../app/python');
 
 describe('python', function(){
   it('test: python', function(){
-    assert.equal(app(),'to test python: done.');
+    assert.equal(app(),'to test python: doing.');
   });
 });
