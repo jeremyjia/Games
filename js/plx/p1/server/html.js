d0947f23f837aac05158d130c7c67012a167134b
littleflute <littleflute@163.com>
2021-11-25 15:40:34 +0800

    51voa.html

--- js/plx/p1/server/html.js
+++ js/plx/p1/server/html.js
@@ -1,6 +1,20 @@
-var tag = "html.js_v0.11";
- 
+var tagHTML = "html";
+var vHTML = "vHTML: v0.11";
+
 var tb = bl$("id_4_tb_server");
 var v = bl$("id_4_v_server");
+ 
+o.getServerFiles(tb,v,tagHTML,cbFun4HTML); 
 
-o.getServerFiles(tb,v,"html");   
+function cbFun4HTML(p1,p2){ 
+    p1.inf.vHTML = vHTML;
+    p1.inf.toDo = function(myView,myBtn){       
+        var s = '<a target="_blank" href="';
+        s += "http://localhost:8080/" + p2;
+        s += '">' + p2;
+        s += '</a>';
+        var htmlToolbar = blo0.blDiv(myView,myView.id+"htmlToolbar", s ,"white");   
+        _on_off_div(myBtn,myView);        
+    } 
+}
+
