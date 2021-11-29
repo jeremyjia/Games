ba6c8be216261383736e175198b323f32c22b0da
Wayne <keep9s@foxmail.com>
2021-11-29 16:00:00 +1100

    update stat_norm.py

--- python/i140/stats_norm.py
+++ python/i140/stats_norm.py
@@ -3,19 +3,24 @@ import argparse
 import numpy as np 
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description='statistics for argparse')  # examplify running by py stats_norm.py -i 2 -o 3 -s -6 -e 3 -t TOY
-parser.add_argument('--mu', '-i', help='mu属性，非必要参数', default=0)
-parser.add_argument('--sigma', '-o',  help='sigma属性，非必要参数', default=1)
-parser.add_argument('--start', '-s',  help='start_point属性，非必要参数', default=-5) 
-parser.add_argument('--end', '-e',  help='end_point属性，非必要参数', default=5)
-parser.add_argument('--title', '-t',  help='x_title属性，非必要参数', default='X AXIS')
+parser = argparse.ArgumentParser(description='statistics for argparse')  # examplify running by py stats_norm.py -i f1.jpg -m 2 -g 3 -s -6 -e 3 -xt TOY -yt SHOP
+parser.add_argument('--filepath', '-i', help='filepath属性，必要参数', required=True)  # 必须是文件名 图片或文档
+parser.add_argument('--output', '-o',  help='file_out 输出文件属性，非必要参数', default='out_stat.jpg') #必须图片名
+parser.add_argument('--mu', '-m', help='mu属性，非必要参数', default=0)  # 均值
+parser.add_argument('--sigma', '-g',  help='sigma属性，非必要参数', default=1)  # 标准差
+parser.add_argument('--start', '-s',  help='start_point属性，非必要参数', default=-5) # 起点
+parser.add_argument('--end', '-e',  help='end_point属性，非必要参数', default=5) # 终点
+parser.add_argument('--xtitle', '-xt',  help='x_title属性，非必要参数', default='X AXIS') # X轴标题
+parser.add_argument('--ytitle', '-yt',  help='y_title属性，非必要参数', default='Y AXIS') # Y轴标题
 args = parser.parse_args()
 
 mu = int(args.mu)  #均值
 sigma = int(args.sigma)  #标准差
 start = int(args.start)  #起点
 end = int(args.end)  #终点
-title = args.title
+x_title = args.xtitle
+y_title = args.ytitle
+output = args.output
 
 # mu = 0  
 # sigma = 1  
@@ -23,9 +28,11 @@ x = np.arange(start, end, 0.1) # form -5 to 5, increase by 0.1
 y = norm.pdf (x, mu, sigma)  # x is horizontal ordinate, norm could be rid by laplace, f, t, gamma, poisson, and as notice below.
 #y = laplace.pdf (x, mu, sigma)
 plt.plot (x, y)
-plt.xlabel(title)
-plt.ylabel('DENSITY')
-plt.show()
+plt.xlabel(x_title)
+plt.ylabel(y_title)
+
+plt.savefig(output)
+# plt.show()
 
 
 # stats连续型随机变量的公共方法
