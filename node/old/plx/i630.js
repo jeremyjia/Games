2c9ad154e5f577c0f537327ea4b09fc04a20f2b0
littleflute <littleflute@163.com>
2022-02-13 22:56:30 +0800

    i630.html

--- node/old/plx/i630.js
+++ node/old/plx/i630.js
@@ -56,7 +56,11 @@ function C4I630(d){
       d.v1 = blo0.blDiv(d,d.id+"v1", tag+"v1", blGrey[0]);
       d.tb.b1 = blo0.blBtn(d.tb,d.tb.id+"b1","b1",blGrey[2]);
       d.tb.b2 = blo0.blBtn(d.tb,d.tb.id+"b2","b2",blGrey[2]);
-      d.tb.b3 = blo0.blBtn(d.tb,d.tb.id+"b3","b3",blGrey[2]);
+      d.tb.b3 = blo0.blBtn(d.tb,d.tb.id+"b3","checkEmailAddress",blGrey[2]);
+      d.tb.b4 = blo0.blBtn(d.tb,d.tb.id+"b4","blo0.blPost",blGrey[2]);
+      d.tb.b5 = blo0.blBtn(d.tb,d.tb.id+"b5","JQUERY_Post-login",blGrey[2]);
+      d.tb.b6 = blo0.blBtn(d.tb,d.tb.id+"b6","JQUERY_Post-checkUserName",blGrey[2]);
+      d.tb.b7 = blo0.blBtn(d.tb,d.tb.id+"b7","JQUERY_Post-m14",blGrey[2]);
   
       var w = {};
       w._2do = function(txt){
@@ -73,5 +77,83 @@ function C4I630(d){
         blo0.blAjx(w,url);//_co.postAJX(url,d.v1);
 
       }
+      d.tb.b4.onclick = function(){       
+        var pl = {
+          "UserName": "q1",
+          "Password": "q1"
+        };
+        var url =  "http://localhost:8080/api/login" ; 
+        blo0.blPOST(url,pl,function(txt){
+         		 d.v1.innerHTML = txt;
+        	});
+      }
+      d.tb.b5.onclick = function(){               
+        var _myPort = 8080;
+        var settings = {
+          "url": "http://localhost:" + _myPort + "/api/login",
+          "method": "POST",
+          "timeout": 0,
+          "headers": {
+              "accept": "application/json",
+              "Content-Type": "application/json"
+          },
+          "data": JSON.stringify({"UserName": "q1","Password":"q1"}),
+        };
+        $.ajax(settings).done(function (response) {  
+            if(response.code==1){  
+              d.v1.innerHTML = "jquery ok: " + JSON.stringify(response);    
+            }
+            else if(0==response.code){
+              d.v1.innerHTML = "code=0:" + JSON.stringify(response);
+            }
+        });   
+      }
+      
+      d.tb.b6.onclick = function(){    
+        var url = "http://localhost:8080/api/checkUserName?UserName=aaa";
+        var settings = {
+          "url": url,
+          "method": "POST",
+          "timeout": 0,
+          "headers": {
+              "accept": "application/json",
+              "Content-Type": "application/json"
+          },
+          "data": JSON.stringify({"UserName": "q1","Password":"q1"}),
+        };
+        $.ajax(settings).done(function (response) {  
+            if(response.code==1){  
+              d.v1.innerHTML = "jquery ok: " + JSON.stringify(response);    
+            }
+            else if(0==response.code){
+              d.v1.innerHTML = "code=0:" + JSON.stringify(response);
+            }
+        });   
+      }
+
+      d.tb.b7.onclick = function(){    
+        var url = "https://jiqie.zhenbi.com/a/re14.php";
+        //url +="?id=%E5%88%98%E5%BE%B7%E5%8D%8E&zhenbi=20191123&id1=905&id2=14&id3=%230000FF&id5=%23FFFFFF";
+
+        var settings = {
+          "url": url,
+          "method": "GET",
+          "timeout": 0,
+          "headers": {
+              "accept": "application/json",
+              "Content-Type": "application/json"
+          },
+          "data": JSON.stringify({"UserName": "q1","Password":"q1"}),
+        };
+        $.ajax(settings).done(function (response) {  
+            if(response.code==1){  
+              d.v1.innerHTML = "jquery ok: " + JSON.stringify(response);    
+            }
+            else if(0==response.code){
+              d.v1.innerHTML = "code=0:" + JSON.stringify(response);
+            }
+        });   
+      }
+
     }
   }
