29791b74bb700e33f2523dc5092c77a70f19e7e6
littleflute <littleflute@163.com>
2022-02-13 12:01:14 +0800

    *

--- node1/app/spider/51voa/index.js
+++ node1/app/spider/51voa/index.js
@@ -9,9 +9,15 @@ e.index = function(req,res){
     request('https://www.51voa.com/',function(error,response,body){
       if(!error && response.statusCode ==200){
         $ = cheerio.load(body);
+
+        s = "<a  href='http://www.baidu.com'>baidu</a><br>";
+        s +="<a href='http://www.sohu.com'>sohu</a>";//as
+        res.send(s);
+        /*
         res.json({
-          'Classnum':$('.list').length
+          'asdf':$('.list').length
         });
+        */
       }
   }); 
 }
