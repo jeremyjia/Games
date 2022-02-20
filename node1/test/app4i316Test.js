0f0e052aba3d3041527ec4e393e71468d8ca8bf9
littleflute <littleflute@163.com>
2022-02-20 22:55:26 +0800

    *

--- node1/test/app4i316Test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const app = require('../app4i316');
-
-describe('app4i316', function(){
-  it('test: app4i316', function(){
-    assert.equal(app(),'to test app4i316: done');
-  });
-});
