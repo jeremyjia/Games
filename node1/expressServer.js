305eeeec0f6a2ad5c4161158ce49ca3ee8fa6f1c
littleflute <littleflute@163.com>
2022-02-26 00:41:57 +0800

    launch+

--- node1/expressServer.js
+++ node1/expressServer.js
@@ -17,44 +17,10 @@ console.log(tag);
 class ExpressServer {
   constructor(port, openApiYaml) {
     this.port = port;
-    this.app = express();
-    
-    this.setupMiddleware();
-  }
+    this.app = express(); 
+  } 
 
-  setupMiddleware() {
-    this.app.use(cors());
-    this.app.use(bodyParser.json());
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: false }));
-    this.app.use(cookieParser());
-    new OpenApiValidator({
-      apiSpecPath: this.openApiPath,
-    }).install(this.app);
- 
- 
-
-
-	 if (!config.PRODUCTION) { // development paths, disable for production
-      this.app.get('/login-redirect', (req, res) => {
-        res.status(200);
-        res.json(req.query);
-      });
-      this.app.get('/oauth2-redirect.html', (req, res) => {
-        res.status(200);
-        res.json(req.query);
-      });
-      this.app.get('/', (req, res) => { 
-        res.sendFile('index.html' , { root : __dirname});        
-      }); 
-      this.app.use('/old',blOld.fOld);
-      this.app.use('/spec', express.static(path.join(__dirname, 'api'))); 
-       
-	  }
-  }
- 
   async launch() {
-    l.tag1(tag,"xd21------")
     return new Promise(
       async (resolve, reject) => {
         try { 
@@ -63,18 +29,10 @@ class ExpressServer {
             resolve(this.server);
           });
         } catch (error) {
-          console.log("xddbg...");     
           reject(error);
         }
       },
     );
-  }
-
-  async close() {
-    if (this.server !== undefined) {
-      await this.server.close();
-      console.log(`Server on port ${this.port} shut down`);
-    }
   }
 }
 
