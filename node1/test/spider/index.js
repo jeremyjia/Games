216f9897d1ac05d56e3fadd5abac43858821d12a
littleflute <littleflute@163.com>
2022-02-25 23:34:29 +0800

    test/xdTest.js*

---
+++ node1/test/spider/index.js
@@ -0,0 +1,7 @@
+const e = {}; 
+module.exports = e;
+
+e.test1 = function(assert){  
+    assert(2 == 1, "test1" );      
+  }
+  
--- node1/test/xdTest.js
+++ node1/test/xdTest.js
@@ -1,7 +1,28 @@
 const assert = require('chai').assert;
+const express = require('express')
+const ESv = express()
+const request = require('supertest');
 const app = require('../app/xd.js');
+const spiderTest = require('./spider/index.js');
 
 describe('xdApp', function(){ 
+  
+  it('spiderTest: test1', function(){     
+    spiderTest.test1(assert);  
+
+    it('endpoint test.', function() {    
+      return request(ESv)
+        .get('/spider')
+        .send()
+        .set('accept', 'application/json') 
+        .expect(200) 
+        .then(response => {   
+          var testMe = response.body.Classnum; 
+          assert(0==testMe,"testMe=" + testMe);
+      }) 
+    });     
+  });
+
   it('xd dev', function(){ 
     assert.equal(app(),'to test xdApp: done.');
   });
