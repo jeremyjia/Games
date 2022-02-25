bee66e5be123295691a21d8414e5a74c0da3e4de
littleflute <littleflute@163.com>
2022-02-25 13:20:02 +0800

    *

--- issues/655/index.js
+++ issues/655/index.js
@@ -14,7 +14,71 @@ function Ci655(_r){
     blo0.blShowObj2Div(v,this);
 
     const _record = function(b,d){
-        d.innerHTML = Date();
+        //blo0.blScript("server.js","https://jeremyjia.github.io/Games/issues/4/server.js");
+        const recordBtn = document.querySelector(".record-btn");
+        const player = document.querySelector(".audio-player");
+
+if (navigator.mediaDevices.getUserMedia) {
+  var chunks = [];
+  const constraints = { audio: true };
+  navigator.mediaDevices.getUserMedia(constraints).then(
+    stream => {
+      console.log("授权成功！");
+
+      const mediaRecorder = new MediaRecorder(stream);
+
+      recordBtn.onclick = () => {
+        if (mediaRecorder.state === "recording") {
+          mediaRecorder.stop();
+          recordBtn.textContent = "record";
+          console.log("录音结束");
+        } else {
+          mediaRecorder.start();
+          console.log("录音中...");
+          recordBtn.textContent = "stop";
+        }
+        console.log("录音器状态：", mediaRecorder.state);
+      };
+
+      mediaRecorder.ondataavailable = e => {
+        chunks.push(e.data);
+      };
+
+      mediaRecorder.onstop = e => {
+        var blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+        chunks = [];
+      
+         let reader= new FileReader();
+         reader.onload = function (e) { 
+             var audioData = e.target.result;
+             alert(audioData);
+             addNewGitHubComment(550, audioData, CallBackSubmit2SixPage);
+              console.log(audioData);
+         }
+         reader.readAsDataURL(blob);
+
+        var audioURL = window.URL.createObjectURL(blob);
+        player.src = audioURL;
+        player.play();
+      };
+    },
+    () => {
+      console.error("授权失败！");
+    }
+  );
+} else {
+  console.error("浏览器不支持 getUserMedia");
+}
+
+        function CallBackSubmit2SixPage(response) {
+            if (response.readyState == 4) {
+                if (response.status == 200 || response.status == 201) {
+                    alert("上传到6号成功!");
+                } else {
+                    alert("Errors, status=" + response.status);
+                }
+            }
+        }
         _on_off_div(b,d);
 		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
     }   
