d249873d2be4b40f03d6a4901e856db96b5bc3b7
littleflute <littleflute@163.com>
2022-02-25 23:54:54 +0800

    blocker: .get('/spider')

--- node1/test/spider/index.js
+++ node1/test/spider/index.js
@@ -2,6 +2,6 @@ const e = {};
 module.exports = e;
 
 e.test1 = function(assert){  
-    assert(2 == 1, "test1" );      
+    assert(1 == 1, "test1" );      
   }
   
--- node1/test/xdTest.js
+++ node1/test/xdTest.js
@@ -8,20 +8,21 @@ const spiderTest = require('./spider/index.js');
 describe('xdApp', function(){ 
   
   it('spiderTest: test1', function(){     
-    spiderTest.test1(assert);  
-
-    it('endpoint test.', function() {    
-      return request(ESv)
-        .get('/spider')
-        .send()
-        .set('accept', 'application/json') 
-        .expect(200) 
-        .then(response => {   
-          var testMe = response.body.Classnum; 
-          assert(0==testMe,"testMe=" + testMe);
-      }) 
-    });     
+    spiderTest.test1(assert);   
   });
+
+  
+  it('endpoint test.', function() {    
+    return request(ESv)
+      .get('/spider')
+      .send()
+      .set('accept', 'application/json') 
+      .expect(200) 
+      .then(response => {   
+        var testMe = response.body.Classnum; 
+        assert(0==testMe,"testMe=" + testMe);
+    }) 
+  });    
 
   it('xd dev', function(){ 
     assert.equal(app(),'to test xdApp: done.');
