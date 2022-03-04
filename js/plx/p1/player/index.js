e2ca94cfda33293bf686f6e841d700477d7022f5
littleflute <littleflute@163.com>
2022-03-05 00:49:18 +0800

    blclass.js*

--- js/plx/p1/player/index.js
+++ js/plx/p1/player/index.js
@@ -1,4 +1,4 @@
-var tag_playerBoard = "index.js_v0.13";
+var tag_playerBoard = "index.js_v0.21";
 var tb = bl$("id_p1_tb"); 
 
 tb.btnPlayerBoard = blo0.blBtn(tb,"id_btnPlayerBoard","btnPlayerBoard",blGrey[2]);
@@ -12,6 +12,7 @@ function CPlayer(){
     this.show = function(b){
         if(!b.v){
             b.v = blo0.blMD("xdTest","xdTest",10,10,100,100,"brown");
+            //blo0.blPlayer(d,"f0Test",originalMp3URL,100,100,400,300,"lightgreen");
         }
         _on_off_div(b,b.v);
         b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];  
