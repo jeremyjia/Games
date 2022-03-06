1ab9437d1e8b80a972ab4b3f38234f34ca0380c9
littleflute <littleflute@163.com>
2022-03-06 13:17:33 +0800

    word.js v0.112

--- js/plx/p1/server/word.js
+++ js/plx/p1/server/word.js
@@ -1,4 +1,4 @@
-var tagWord = "word_v0.111"; 
+var tagWord = "word_v0.112"; 
 
 var tb = bl$("id_4_tb_server");
 var v = bl$("id_4_v_server");
@@ -6,6 +6,35 @@ var v = bl$("id_4_v_server");
 const ow = new CWord(tb,v);
 
 function CWord(_tb,_v){    
+    this.blr_test_json = function(b,d){
+        if(!d.v){
+            d.v = blo0.blDiv(d,d.id+"v","v","Grey"); 
+            var ta 	= blo0.blTextarea(d.v,d.v+"ta","ta...","lightblue");
+            ta.style.width="95%"; 
+            ta.style.height="30px"; 
+            var tb = blo0.blDiv(d.v,d.v+"tb","tb",blGrey[0]);    
+            var v = blo0.blDiv(d.v,d.v+"v","v","Gray");             
+            var b1 = blo0.blBtn(tb,tb.id+"b1","b1",blGrey[1]);
+            
+            b1.onclick = function(){       
+                var _data = {};
+                _data.version = "v0.11"; 
+                var fileName = blo0.blDate();         
+                var url = "http://localhost:8080/json?fileName=" + fileName + ".json"; 
+                blo0.blPOST(url,_data,function(txt){
+                    
+                    var sHTML = "<a target='_blank' href='http://localhost:8080/"+fileName+".docx'>"+fileName+".docx</a>";
+                    v.innerHTML = sHTML;
+
+                });
+            }            
+        }
+        
+        _on_off_div(b,d);
+		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
+
+    }
+    this.bll0= "--0--";
     this.blrsave2Word = function(b,d){
         if(!d.v){
             d.v = blo0.blDiv(d,d.id+"v","v","Grey"); 
