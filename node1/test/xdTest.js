2d6f8f37cc4c341750ba058eddb4455d45023cb5
littleflute <littleflute@163.com>
2022-03-12 10:25:21 +0800

    done

--- node1/test/xdTest.js
+++ node1/test/xdTest.js
@@ -31,6 +31,6 @@ describe(tag4xdTest, function(){
   });    
 
   it('xd dev', function(){ 
-    assert.equal(app(),'to test xdApp: done.*637');
+    assert.equal(app(),'to test xdApp: done.');
   });
 });
