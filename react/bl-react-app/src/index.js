b12117919cf822725cef07b0511e725478eebe70
littleflute <littleflute@163.com>
2022-03-15 17:29:26 +0800

    src/App.js*

--- react/bl-react-app/src/index.js
+++ react/bl-react-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
- 
-//import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
