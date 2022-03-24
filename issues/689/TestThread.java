723323d5f0ffaf691f7fe419c5b53c39edd828f8
littleflute <littleflute@163.com>
2022-03-24 22:56:53 +0800

    deadlock resolved

--- issues/689/TestThread.java
+++ issues/689/TestThread.java
@@ -26,14 +26,14 @@ public class TestThread {
    }
    private static class ThreadDemo2 extends Thread {
       public void run() {
-         synchronized (Lock2) {
-            System.out.println("Thread 2: Holding lock 2...");
+         synchronized (Lock1) {
+            System.out.println("Thread 2: Holding lock 1...");
             
             try { Thread.sleep(10); }
             catch (InterruptedException e) {}
-            System.out.println("Thread 2: Waiting for lock 1...");
+            System.out.println("Thread 2: Waiting for lock 2...");
             
-            synchronized (Lock1) {
+            synchronized (Lock2) {
                System.out.println("Thread 2: Holding lock 1 & 2...");
             }
          }
