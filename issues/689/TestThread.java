75029874eafaee95b3a6ebc6d7542561016d3a14
littleflute <littleflute@163.com>
2022-03-24 23:12:04 +0800

    thread control

--- issues/689/TestThread.java
+++ issues/689/TestThread.java
@@ -1,42 +1,82 @@
+class RunnableDemo implements Runnable {
+   public Thread t;
+   private String threadName;
+   boolean suspended = false;
+
+   RunnableDemo( String name) {
+      threadName = name;
+      System.out.println("Creating " +  threadName );
+   }
+   
+   public void run() {
+      System.out.println("Running " +  threadName );
+      try {
+         for(int i = 10; i > 0; i--) {
+            System.out.println("Thread: " + threadName + ", " + i);
+            // Let the thread sleep for a while.
+            Thread.sleep(300);
+            synchronized(this) {
+               while(suspended) {
+                  wait();
+               }
+            }
+         }
+      } catch (InterruptedException e) {
+         System.out.println("Thread " +  threadName + " interrupted.");
+      }
+      System.out.println("Thread " +  threadName + " exiting.");
+   }
+
+   public void start () {
+      System.out.println("Starting " +  threadName );
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+   
+   void suspend() {
+      suspended = true;
+   }
+   
+   synchronized void resume() {
+      suspended = false;
+      notify();
+   }
+}
+
 public class TestThread {
-   public static Object Lock1 = new Object();
-   public static Object Lock2 = new Object();
-   
+
    public static void main(String args[]) {
-      ThreadDemo1 T1 = new ThreadDemo1();
-      ThreadDemo2 T2 = new ThreadDemo2();
-      T1.start();
-      T2.start();
-   }
-   
-   private static class ThreadDemo1 extends Thread {
-      public void run() {
-         synchronized (Lock1) {
-            System.out.println("Thread 1: Holding lock 1...");
-            
-            try { Thread.sleep(10); }
-            catch (InterruptedException e) {}
-            System.out.println("Thread 1: Waiting for lock 2...");
-            
-            synchronized (Lock2) {
-               System.out.println("Thread 1: Holding lock 1 & 2...");
-            }
-         }
+
+      RunnableDemo R1 = new RunnableDemo( "Thread-1");
+      R1.start();
+
+      RunnableDemo R2 = new RunnableDemo( "Thread-2");
+      R2.start();
+
+      try {
+         Thread.sleep(1000);
+         R1.suspend();
+         System.out.println("Suspending First Thread");
+         Thread.sleep(1000);
+         R1.resume();
+         System.out.println("Resuming First Thread");
+         
+         R2.suspend();
+         System.out.println("Suspending thread Two");
+         Thread.sleep(1000);
+         R2.resume();
+         System.out.println("Resuming thread Two");
+      } catch (InterruptedException e) {
+         System.out.println("Main thread Interrupted");
+      }try {
+         System.out.println("Waiting for threads to finish.");
+         R1.t.join();
+         R2.t.join();
+      } catch (InterruptedException e) {
+         System.out.println("Main thread Interrupted");
       }
+      System.out.println("Main thread exiting.");
    }
-   private static class ThreadDemo2 extends Thread {
-      public void run() {
-         synchronized (Lock1) {
-            System.out.println("Thread 2: Holding lock 1...");
-            
-            try { Thread.sleep(10); }
-            catch (InterruptedException e) {}
-            System.out.println("Thread 2: Waiting for lock 2...");
-            
-            synchronized (Lock2) {
-               System.out.println("Thread 2: Holding lock 1 & 2...");
-            }
-         }
-      }
-   } 
 }
