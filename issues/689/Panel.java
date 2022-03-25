cb3ff1b92b9edc5310933acd1df198593922f7fa
littleflute <littleflute@163.com>
2022-03-25 11:37:46 +0800

    Panel.java: pie chart

--- issues/689/Panel.java
+++ issues/689/Panel.java
@@ -1,23 +1,50 @@
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
-public class Panel { 
-   public static void main(String[] args) { 
-      SwingUtilities.invokeLater(new Runnable() { 
-         public void run() { 
-            displayJFrame();
-         } 
-      }); 
+class Part {
+   double value;
+   Color color;
+   
+   public Part(double value, Color color) {
+      this.value = value;
+      this.color = color;
+   }
+}
+class MyComponent extends JComponent {
+   Part[] slices = { 
+      new Part(15, Color.yellow), new Part(30, Color.white), new Part(25, Color.blue), new Part(30, Color.red) 
+   }; 
+   MyComponent() {
+   }
+   public void paint(Graphics g) {
+      drawPie((Graphics2D) g, getBounds(), slices);
    } 
-   static void displayJFrame() { 
-      JFrame frame = new JFrame("Tutorialspoint");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setBackground(Color.red);
-      frame.setPreferredSize(new Dimension(400, 300));
-      frame.pack();
-      frame.setLocationRelativeTo(null);
+   void drawPie(Graphics2D g, Rectangle area, Part[] slices) {
+      double total = 0.0D;
+      for (int i = 0; i < slices.length; i++) {
+         total += slices[i].value;
+      } 
+      double curValue = 0.0D;
+      int startAngle = 0;
+      for (int i = 0; i < slices.length; i++) {
+         startAngle = (int) (curValue * 360 / total);
+         int arcAngle = (int) (slices[i].value * 360 / total);
+         
+         g.setColor(slices[i].color);
+         g.fillArc(area.x, area.y, area.width, area.height, startAngle, arcAngle);
+         curValue += slices[i].value;
+      } 
+   }
+}
+public class Panel {
+   public static void main(String[] argv) {
+      JFrame frame = new JFrame();
+      frame.getContentPane().add(new MyComponent());
+      frame.setSize(300, 200);
       frame.setVisible(true);
    }
 }
