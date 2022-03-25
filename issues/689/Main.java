cc8e8ab0070f310f917518dd6e054a791e36d111
littleflute <littleflute@163.com>
2022-03-25 11:32:01 +0800

    Main.java: VALUE_ANTIALIAS_ON

--- issues/689/Main.java
+++ issues/689/Main.java
@@ -1,13 +1,24 @@
-import java.awt.*;
-import java.awt.image.MemoryImageSource;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
 public class Main {
-   public static void main(String[] argv) throws Exception {
-      int[] pixels = new int[16 * 16];
-      Image image = Toolkit.getDefaultToolkit().createImage(
-         new MemoryImageSource(16, 16, pixels, 0, 16));
-      Cursor transparentCursor = Toolkit.getDefaultToolkit().createCustomCursor(
-         image, new Point(0, 0), "invisibleCursor");
-      System.out.println("Transparent Cursor created.");
+   public static void main(String[] args) {
+      JFrame frame = new JFrame();
+      frame.add(new MyComponent());
+      frame.setSize(300, 300);
+      frame.setVisible(true);
+   }
+}
+class MyComponent extends JComponent {
+   public void paint(Graphics g) {
+      Graphics2D g2 = (Graphics2D) g;
+      RenderingHints rh = g2.getRenderingHints();
+      boolean bl = rh.containsValue (RenderingHints.VALUE_ANTIALIAS_ON);
+      System.out.println(bl);
+      g2.setRenderingHint(
+         RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
    }
 }
