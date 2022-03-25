3e489f0082909e42858fba69c610ae7035424277
littleflute <littleflute@163.com>
2022-03-25 11:34:45 +0800

    Pane.java: color: bug?

--- issues/689/Panel.java
+++ issues/689/Panel.java
@@ -1,11 +1,23 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class Panel {
-   public static void main(String[] argv) throws Exception {
-      JFrame frame = new JFrame();
-      frame.setCursor(frame.getToolkit().createCustomCursor(
-         new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),"null"));
+public class Panel { 
+   public static void main(String[] args) { 
+      SwingUtilities.invokeLater(new Runnable() { 
+         public void run() { 
+            displayJFrame();
+         } 
+      }); 
+   } 
+   static void displayJFrame() { 
+      JFrame frame = new JFrame("Tutorialspoint");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setBackground(Color.red);
+      frame.setPreferredSize(new Dimension(400, 300));
+      frame.pack();
+      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
    }
 }
