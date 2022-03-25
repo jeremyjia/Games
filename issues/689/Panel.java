a4e2970e88ad99ba097cdb83ae4de9b554ccca25
littleflute <littleflute@163.com>
2022-03-25 11:28:52 +0800

    Panel.java: cursor

--- issues/689/Panel.java
+++ issues/689/Panel.java
@@ -1,23 +1,11 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Panel extends JPanel {
-   public void paintComponent(Graphics g) {
-      super.paintComponent(g);
-      Graphics2D g2d = (Graphics2D) g;
-      g2d.setColor(new Color(31, 21, 1));
-      g2d.fillRect(250, 195, 90, 60);
-   } 
-   public static void main(String[] args) {
-      Panel rects = new Panel();
-      JFrame frame = new JFrame("Rectangles");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.add(rects);
-      frame.setSize(360, 300);
-      frame.setLocationRelativeTo(null);
-      frame.setVisible(true);
+public class Panel {
+   public static void main(String[] argv) throws Exception {
+      JFrame frame = new JFrame();
+      frame.setCursor(frame.getToolkit().createCustomCursor(
+         new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),"null"));
    }
 }
