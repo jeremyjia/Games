f2676fec72a269f02126276bd6a7ef5f9628079d
littleflute <littleflute@163.com>
2022-03-25 13:57:16 +0800

    jdbcConn.java*

--- issues/689/jdbcConn.java
+++ issues/689/jdbcConn.java
@@ -2,16 +2,25 @@ import java.sql.*;
 
 public class jdbcConn {
    public static void main(String[] args) {
-      try{  
-         Class.forName("com.mysql.jdbc.Driver");  
-         Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/g6DB","root","password");  
-         //here sonoo is database name, root is username and password  
-         Statement stmt=con.createStatement();  
-         ResultSet rs=stmt.executeQuery("select * from Group6User1s");  
-         while(rs.next())  
-         System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-         con.close();  
-         }catch(Exception e){ System.out.println(e);}  
-         }  
+      try {
+         Class.forName("com.mysql.jdbc.Driver");
+      } catch(ClassNotFoundException e) {
+         System.out.println("Class not found "+ e);
+      }
+      System.out.println("JDBC Class found");
+      int no_of_rows = 0;
+      
+      try {
+         Connection con = DriverManager.getConnection (
+            "jdbc:mysql://localhost:3306/g6DB","root", "password");  
+         Statement stmt = con.createStatement();
+         ResultSet rs = stmt.executeQuery ("SELECT * FROM Group6User1s");
+         while (rs.next()) {
+            no_of_rows++;
+         }
+         System.out.println("There are "+ no_of_rows + " record in the table");
+      } catch(SQLException e){
+         System.out.println("SQL exception occured: e = " + e);
+      }
    }
 }
