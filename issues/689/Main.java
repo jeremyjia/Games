25c153d184ff3b47e2f2f708e17861c75fb520ca
littleflute <littleflute@163.com>
2022-03-25 11:15:49 +0800

    java: polygon

--- issues/689/Main.java
+++ issues/689/Main.java
@@ -1,35 +1,28 @@
 import java.awt.*;
-import java.awt.font.FontRenderContext;
-import java.awt.geom.Rectangle2D;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Main extends JPanel {
-   public void paint(Graphics g) {
-      Graphics2D g2 = (Graphics2D) g;
-      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      g2.setFont(new Font("Serif", Font.PLAIN, 48));
-      paintHorizontallyCenteredText(g2, "Java Source", 200, 75);
-      paintHorizontallyCenteredText(g2, "and", 200, 125);
-      paintHorizontallyCenteredText(g2, "Support", 200, 175);
-   }
-   protected void paintHorizontallyCenteredText(
-      Graphics2D g2, String s, float centerX, float baselineY) {
-      
-      FontRenderContext frc = g2.getFontRenderContext();
-      Rectangle2D bounds = g2.getFont().getStringBounds(s, frc);
-      float width = (float) bounds.getWidth();
-      g2.drawString(s, centerX - width / 2, baselineY);
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      Polygon p = new Polygon();
+      for (int i = 0; i < 5; i++) p.addPoint((int) (
+         100 + 50 * Math.cos(i * 2 * Math.PI / 5)),(int) (100 + 50 * Math.sin(
+         i * 2 * Math.PI / 5)));
+      g.drawPolygon(p);
    }
    public static void main(String[] args) {
-      Frame f = new Frame("Tutorialspoint");
-      Component text = new TextArea("Sairamkrishna Mamamahe");
-      Component button = new Button("Button");
-      f.add(text, BorderLayout.NORTH);
-      f.add(button, BorderLayout.SOUTH);
-      int width = 300;
-      int height = 300;
-      f.setSize(width, height);
-      f.setVisible(true);
+      JFrame frame = new JFrame();
+      frame.setTitle("Polygon");
+      frame.setSize(350, 250);
+      
+      frame.addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent e) {
+            System.exit(0);
+         }
+      });
+      Container contentPane = frame.getContentPane();
+      contentPane.add(new Main());
+      frame.setVisible(true);
    }
 }
