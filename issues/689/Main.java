c892dac14c1595f12918486b65183f4d1b6d8a79
littleflute <littleflute@163.com>
2022-03-25 11:33:14 +0800

    Main.java: color

--- issues/689/Main.java
+++ issues/689/Main.java
@@ -1,24 +1,44 @@
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-public class Main {
-   public static void main(String[] args) {
-      JFrame frame = new JFrame();
-      frame.add(new MyComponent());
-      frame.setSize(300, 300);
-      frame.setVisible(true);
+public class Main extends JComponent {
+   BufferedImage image;
+   public void initialize() {
+      int width = getSize().width;
+      int height = getSize().height;
+      int[] data = new int[width * height];
+      int index = 0;
+      
+      for (int i = 0; i < height; i++) {
+         int red = (i * 255) / (height - 1);
+         for (int j = 0; j < width; j++) {
+            int green = (j * 255) / (width - 1);
+            int blue = 128;
+            data[index++] = (red<<16) | (green<<8) | blue;
+         }
+      }
+      image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+      image.setRGB(0, 0, width, height, data, 0, width);
    }
-}
-class MyComponent extends JComponent {
    public void paint(Graphics g) {
-      Graphics2D g2 = (Graphics2D) g;
-      RenderingHints rh = g2.getRenderingHints();
-      boolean bl = rh.containsValue (RenderingHints.VALUE_ANTIALIAS_ON);
-      System.out.println(bl);
-      g2.setRenderingHint(
-         RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      if (image == null)
+      initialize();
+      g.drawImage(image, 0, 0, this);
+   }
+   public static void main(String[] args) {
+      JFrame f = new JFrame("Display Colours");
+      f.getContentPane().add(new Main());
+      f.setSize(300, 300);
+      f.setLocation(100, 100);
+      f.addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent e) {
+            System.exit(0);
+         }
+      });
+      f.setVisible(true);
    }
 }
