efda365e9428e916af922731ec08d239fb79313d
littleflute <littleflute@163.com>
2022-03-25 11:11:00 +0800

    frame java

--- issues/689/Main.java
+++ issues/689/Main.java
@@ -1,36 +1,30 @@
-import java.awt.*; 
-import java.awt.event.*;
-import java.awt.geom.Line2D;
-import javax.swing.JApplet;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Main extends JApplet {
-   public void init() {
-      setBackground(Color.white);
-      setForeground(Color.white);
-   }
+public class Main extends JPanel {
    public void paint(Graphics g) {
       Graphics2D g2 = (Graphics2D) g;
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      g2.setPaint(Color.gray);
-      int x = 5;
-      int y = 7;
-      g2.draw(new Line2D.Double(x, y, 200, 200));
-      g2.drawString("Line", x, 250);
+      g2.setFont(new Font("Serif", Font.PLAIN, 48));
+      paintHorizontallyCenteredText(g2, "Java Source", 200, 75);
+      paintHorizontallyCenteredText(g2, "and", 200, 125);
+      paintHorizontallyCenteredText(g2, "Support", 200, 175);
    }
-   public static void main(String s[]) {
-      JFrame f = new JFrame("Line");
-      f.addWindowListener(new WindowAdapter() {
-         public void windowClosing(WindowEvent e) {
-            System.exit(0);
-         }
-      });
-      JApplet applet = new Main();
-      f.getContentPane().add("Center", applet);
-      applet.init();
+   protected void paintHorizontallyCenteredText(
+      Graphics2D g2, String s, float centerX, float baselineY) {
       
-      f.pack();
-      f.setSize(new Dimension(300, 300));
+      FontRenderContext frc = g2.getFontRenderContext();
+      Rectangle2D bounds = g2.getFont().getStringBounds(s, frc);
+      float width = (float) bounds.getWidth();
+      g2.drawString(s, centerX - width / 2, baselineY);
+   }
+   public static void main(String[] args) {
+      JFrame f = new JFrame();
+      f.getContentPane().add(new Main());
+      f.setSize(450, 350);
       f.setVisible(true);
    }
 }
