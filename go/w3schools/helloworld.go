f31cd88590fde47dabfe42706cce9609ec2183eb
littleflute <littleflute@163.com>
2022-03-31 21:33:19 +0800

    for

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -2,18 +2,7 @@ package main
 import ("fmt")
 
 func main() {
-  numbers := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
-  // Original slice
-  fmt.Printf("numbers = %v\n", numbers)
-  fmt.Printf("length = %d\n", len(numbers))
-  fmt.Printf("capacity = %d\n", cap(numbers))
-
-  // Create copy with only needed numbers
-  neededNumbers := numbers[:len(numbers)-10]
-  numbersCopy := make([]int, len(neededNumbers))
-  copy(numbersCopy, neededNumbers)
-
-  fmt.Printf("numbersCopy = %v\n", numbersCopy)
-  fmt.Printf("length = %d\n", len(numbersCopy))
-  fmt.Printf("capacity = %d\n", cap(numbersCopy))
+  for i:=0; i <= 100; i+=10 {
+    fmt.Println(i)
+  }
 }
