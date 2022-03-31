11b0c1e4e2f698eaa9d2650525b56aab682ca789
littleflute <littleflute@163.com>
2022-03-31 20:56:20 +0800

    *|

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -1,11 +1,12 @@
 package main
 import ("fmt")
 
-func main() {
-  var a string
-  var b int
-  var c bool
+var a int
+var b int = 2
+var c = 3
 
+func main() {
+  a = 1
   fmt.Println(a)
   fmt.Println(b)
   fmt.Println(c)
