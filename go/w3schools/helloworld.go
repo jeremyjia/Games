cdd00b27720b0ffc11e633bd993ce241d826cc84
littleflute <littleflute@163.com>
2022-03-31 20:53:04 +0800

    *

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -2,5 +2,11 @@ package main
 import ("fmt")
 
 func main() {
-  fmt.Println("Hello World!")
+  var student1 string = "John" //type is string
+  var student2 = "Jane" //type is inferred
+  x := 2 //type is inferred
+
+  fmt.Println(student1)
+  fmt.Println(student2)
+  fmt.Println(x)
 }
