febe25d281df7f97bc14a7eaf31f40e825c2f65b
littleflute <littleflute@163.com>
2022-03-31 21:20:50 +0800

    slice

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -2,9 +2,13 @@ package main
 import ("fmt")
 
 func main() {
-  var arr1 = [3]int{1,2,3}
-  arr2 := [5]int{4,5,6,7,8}
+  myslice1 := []int{}
+  fmt.Println(len(myslice1))
+  fmt.Println(cap(myslice1))
+  fmt.Println(myslice1)
 
-  fmt.Println(arr1)
-  fmt.Println(arr2)
+  myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+  fmt.Println(len(myslice2))
+  fmt.Println(cap(myslice2))
+  fmt.Println(myslice2)
 }
