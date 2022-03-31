e9249da91aa5a24fced2e448a3f524728f7a9cd7
littleflute <littleflute@163.com>
2022-03-31 21:26:38 +0800

    copy

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -2,13 +2,18 @@ package main
 import ("fmt")
 
 func main() {
-  myslice1 := []int{}
-  fmt.Println(len(myslice1))
-  fmt.Println(cap(myslice1))
-  fmt.Println(myslice1)
+  numbers := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+  // Original slice
+  fmt.Printf("numbers = %v\n", numbers)
+  fmt.Printf("length = %d\n", len(numbers))
+  fmt.Printf("capacity = %d\n", cap(numbers))
 
-  myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
-  fmt.Println(len(myslice2))
-  fmt.Println(cap(myslice2))
-  fmt.Println(myslice2)
+  // Create copy with only needed numbers
+  neededNumbers := numbers[:len(numbers)-10]
+  numbersCopy := make([]int, len(neededNumbers))
+  copy(numbersCopy, neededNumbers)
+
+  fmt.Printf("numbersCopy = %v\n", numbersCopy)
+  fmt.Printf("length = %d\n", len(numbersCopy))
+  fmt.Printf("capacity = %d\n", cap(numbersCopy))
 }
