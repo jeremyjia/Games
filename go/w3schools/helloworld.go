502e3cc8d9440ec6cf97e6c5d851565ecf4406d9
littleflute <littleflute@163.com>
2022-03-31 21:50:07 +0800

    map

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -1,38 +1,15 @@
 package main
 import ("fmt")
 
-type Person struct {
-  name string
-  age int
-  job string
-  salary int
-}
-
 func main() {
-  var pers1 Person
-  var pers2 Person
+  var a = make(map[string]string)
+  a["brand"] = "Ford"
+  a["model"] = "Mustang"
+  a["year"] = "1964"
 
-  // Pers1 specification
-  pers1.name = "Hege"
-  pers1.age = 45
-  pers1.job = "Teacher"
-  pers1.salary = 6000
+  fmt.Println(a)
 
-  // Pers2 specification
-  pers2.name = "Cecilie"
-  pers2.age = 24
-  pers2.job = "Marketing"
-  pers2.salary = 4500
+  delete(a,"year")
 
-  // Access and print Pers1 info
-  fmt.Println("Name: ", pers1.name)
-  fmt.Println("Age: ", pers1.age)
-  fmt.Println("Job: ", pers1.job)
-  fmt.Println("Salary: ", pers1.salary)
-
-  // Access and print Pers2 info
-  fmt.Println("Name: ", pers2.name)
-  fmt.Println("Age: ", pers2.age)
-  fmt.Println("Job: ", pers2.job)
-  fmt.Println("Salary: ", pers2.salary)
+  fmt.Println(a)
 }
