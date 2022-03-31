63d3a741791111f79f6d1decd934e2ddd961bbc1
littleflute <littleflute@163.com>
2022-03-31 21:03:53 +0800

    *

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -1,14 +1,8 @@
 package main
 import ("fmt")
 
-func main() {
-   var (
-     a int
-     b int = 1
-     c string = "hello"
-   )
+const PI = 3.14
 
-  fmt.Println(a)
-  fmt.Println(b)
-  fmt.Println(c)
+func main() {
+  fmt.Println(PI)
 }
