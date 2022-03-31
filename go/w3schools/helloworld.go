11748f630e4e81457c1c50dbd5a467bdfe598882
littleflute <littleflute@163.com>
2022-03-31 21:17:23 +0800

    arr

--- go/w3schools/helloworld.go
+++ go/w3schools/helloworld.go
@@ -2,12 +2,9 @@ package main
 import ("fmt")
 
 func main() {
-  var txt = "Hello"
- 
-  fmt.Printf("%s\n", txt)
-  fmt.Printf("%q\n", txt)
-  fmt.Printf("%18s\n", txt)
-  fmt.Printf("%-8s\n", txt)
-  fmt.Printf("%x\n", txt)
-  fmt.Printf("% x\n", txt)
+  var arr1 = [3]int{1,2,3}
+  arr2 := [5]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
 }
