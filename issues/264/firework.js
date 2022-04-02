c169360c7801c4ffa581cb3b4f095b30f5d966ee
littleflute <littleflute@163.com>
2022-04-02 17:54:21 +0800

    i264*

--- issues/264/firework.js
+++ issues/264/firework.js
@@ -1,3 +1,33 @@
+class Vector {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	add(vec) {
+		this.x += vec.x;
+		this.y += vec.y;
+	}
+
+	sub(vec) {
+		this.x -= vec.x;
+		this.y -= vec.y;
+	}
+
+	mult(value) {
+		this.x *= value;
+		this.y *= value;
+	}
+
+	static randomVector() {
+		let angle = random(0, 360) * Math.PI / 180;
+		let len = random(1, 5);
+		let x = Math.cos(angle) * len;
+		let y = Math.sin(angle) * len;
+		return new Vector(x, y);
+	}
+}
+
 class Firework {
 	constructor() {
 		let vel = new Vector(
