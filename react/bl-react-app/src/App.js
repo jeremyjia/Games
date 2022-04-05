d165045ab85109bf92b5494b34c416be684c2248
littleflute <littleflute@163.com>
2022-04-05 10:05:57 +0800

    About.js

--- react/bl-react-app/src/App.js
+++ react/bl-react-app/src/App.js
@@ -7,11 +7,10 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import './App.css';
-import MainApp from './Main/MainApp.js';
+import About from './Components/About.js';
 
 const Home = () => <span>Home</span>;
-
-const About = () => <span>About</span>;
+ 
 
 const Users = () => <span>Users</span>;
 
@@ -19,21 +18,7 @@ const App = () => (
   <MemoryRouter>
     <Container className="p-3">
       <Container className="p-5 mb-4 bg-light rounded-3">
-        <h1 className="header">Welcome To React-Bootstrap v0.21</h1>
-        <h2>
-          Current Page is{' '}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </h2>
+        <h1 className="header">Welcome To React-Bootstrap v0.22</h1>
         <h2>
           Navigate to{' '}
           <ButtonToolbar className="custom-btn-toolbar">
@@ -48,13 +33,23 @@ const App = () => (
             </LinkContainer>
           </ButtonToolbar>
         </h2>
+        <h2>
+          Current Page is{' '}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </h2>
+        
       </Container>
 
-      <Container>
-      <div className="App">
-        <MainApp />
-      </div>
-      </Container>
     </Container>
     
   </MemoryRouter>
