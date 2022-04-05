1bf7d2ee6ccc13f8459ef495c21bf44a9fc2fc9d
littleflute <littleflute@163.com>
2022-04-05 13:08:02 +0800

    blog*

--- react/bl-react-app/src/Components/w3/blog/index.js
+++ react/bl-react-app/src/Components/w3/blog/index.js
@@ -1,11 +1,15 @@
  
 import '../../../css/w3.css';
 import Header from './header.js';
+import Body from './body.js';
+import Footer from './footer.js';
 
 const W3Blog = () => (    
     <body class="w3-light-grey"> 
         <div class="w3-content" style={{maxWidth:"1400px"}}> 
             <Header />
+            <Body />
+            <Footer />
         </div>         
     </body>
 );
