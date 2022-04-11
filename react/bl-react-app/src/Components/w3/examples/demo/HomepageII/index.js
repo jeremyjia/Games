02a7322b68863d8ceb544c2bfc0c3f579e3a62ed
littleflute <littleflute@163.com>
2022-04-11 20:49:02 +0800

    p1ui5*

--- react/bl-react-app/src/Components/w3/examples/demo/HomepageII/index.js
+++ react/bl-react-app/src/Components/w3/examples/demo/HomepageII/index.js
@@ -1,48 +1,56 @@
  
 import { MemoryRouter, Switch, Route } from 'react-router-dom';
-
-import Container from 'react-bootstrap/Container';
+ 
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import { LinkContainer } from 'react-router-bootstrap';
   
 import '../../../4/w3.css';
-import Left1 from './left1.js';
+import Users from './Users.js';
 import Main from './main.js'; 
+import Home from './home.js';
 
 const W3ExampleDemoHomepageII = () => (     
   <MemoryRouter> 
         <div class="w3-sidebar w3-collapse w3-white w3-animate-left w3-large" style={{border:"solid 1px red", width:"300px;"}}>
-          <h1 className="header">Welcome To appHome v0.112</h1>
-          <h2> 
-              Navigate to{' '}
+      
+          <h2>  
+            <div>
               <ButtonToolbar className="custom-btn-toolbar">
-              <LinkContainer to="/">
-                  <Button>Home</Button>
-              </LinkContainer>
-              <LinkContainer to="/about">
-                  <Button>About</Button>
-              </LinkContainer>
-              <LinkContainer to="/users">
-                  <Button>Users</Button>
-              </LinkContainer>
+                <LinkContainer to="/">
+                    <Button>Home</Button>
+                </LinkContainer>  
               </ButtonToolbar>
+            </div>
+            <div>
+              <ButtonToolbar className="custom-btn-toolbar"> 
+                <LinkContainer to="/main">
+                    <Button>Main</Button>
+                </LinkContainer> 
+              </ButtonToolbar>
+            </div>
+            <div>
+              <ButtonToolbar className="custom-btn-toolbar">  
+                <LinkContainer to="/users">
+                    <Button>Users</Button>
+                </LinkContainer>
+              </ButtonToolbar>
+            </div>
           </h2>
         </div>
 
-        <div class="w3-main" style={{marginLeft:"100px", border:"solid 1px purple"}}> 
-          <Main />
-          <h2>
-              Current Page is{' '}
+        <div class="w3-main" style={{marginLeft:"10px", border:"solid 1px purple"}}> 
+       
+          <h2> 
               <Switch>
-                  <Route path="/about">
-                      <div>about</div>
-                  </Route>
                   <Route path="/users">
-                      <div>users</div>
+                      <Users />
+                  </Route>
+                  <Route path="/main">
+                      <Main />
                   </Route>
                   <Route path="/">
-                      <div>Home</div>
+                      <Home />
                   </Route>
               </Switch>
           </h2>
