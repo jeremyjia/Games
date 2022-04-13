f06b13639683555f689b436a367920c455f30fa2
littleflute <littleflute@163.com>
2022-04-13 20:26:49 +0800

    *

--- node1/public/zhipu/blNotes.js
+++ node1/public/zhipu/blNotes.js
@@ -3,8 +3,16 @@ var _notes = function(lsNotes,x,y,dx,cbFun){
         var s = "";
         s += cbFun("notes: v0.11",111, 11,"yellow");
         var l = lsNotes;
+
         for(i in l){
-          s += cbFun(l[i],x +  i*dx , y,"brown");
+          var nt = l[i].split(',');
+
+          var ii = "["
+          if(nt.length>1){
+              ii += 
+          }
+          ii +="]";
+          s += cbFun(ii, x +  i*dx , y,"brown");
         }
         return s;
 }
