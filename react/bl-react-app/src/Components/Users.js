6cab978d1e08433907a1a699ab1dc3ed57c8cd67
littleflute <littleflute@163.com>
2022-04-19 19:17:51 +0800

    *

--- react/bl-react-app/src/Components/Users.js
+++ react/bl-react-app/src/Components/Users.js
@@ -1,8 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import { memo } from "react";
 import Container from 'react-bootstrap/Container';
 import PropTypes from "prop-types";
  
 import Car from "./w3/Tutorial/Car.js"
+
+const Todos = ({ todos, addTodo }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+      <button onClick={addTodo}>Add Todo</button>
+    </>
+  );
+};
+
+const AppUseCallback = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
 
 function Example() {
   const [count, setCount] = useState(0);
@@ -83,6 +120,7 @@ const Users = () => (
       <Counter />
       <Example />
       <div className="App">
+        <AppUseCallback />
         <ItemList />
       </div>
       <Car xd="1973" />
