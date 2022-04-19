cc5658974443e8a1b3799596eca01c16a36b6d86
littleflute <littleflute@163.com>
2022-04-19 19:35:53 +0800

    *

--- react/bl-react-app/src/Components/Users.js
+++ react/bl-react-app/src/Components/Users.js
@@ -16,43 +16,23 @@ const Todos = ({ todos, addTodo }) => {
       <button onClick={addTodo}>Add Todo</button>
     </>
   );
-};
-const expensiveCalculation = (num) => {
-  console.log("Calculating...");
-  for (let i = 0; i < 1000000000; i++) {
-    num += 1;
-  }
-  return num;
-};
+}; 
 const AppUseCallback = () => {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]); 
-  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+  const [data, setData] = useState(null);
 
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
-  const addTodo = () => {
-    setTodos((t) => [...t, "New Todo"]);
-  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+ }, []);
 
   return (
-    <div>
-      <div>
-        <h2>My Todos</h2>
-        {todos.map((todo, index) => {
-          return <p key={index}>{todo}</p>;
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}> xd::: {item.title}</p>;
         })}
-        <button onClick={addTodo}>Add Todo</button>
-      </div>
-      <hr />
-      <div>
-        Count: {count}
-        <button onClick={increment}>+</button>
-        <h2>Expensive Calculation</h2>
-        {calculation}
-      </div>
-    </div>
+    </>
   );
 };
 
