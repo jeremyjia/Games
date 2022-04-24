c60caf9484e896d7ad4ef5f112c2a03da79ae56b
littleflute <littleflute@163.com>
2022-04-24 23:23:15 +0800

    main.go 9091

--- go/main.go
+++ go/main.go
@@ -23,7 +23,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
  
 func main() {
 	http.HandleFunc("/", sayhelloName) //设置访问的路由
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	err := http.ListenAndServe(":9091", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
