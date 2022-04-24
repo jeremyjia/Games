1d2d91f6c3fb30d3d3f55061d5b0d9425f23299a
littleflute <littleflute@163.com>
2022-04-24 23:48:53 +0800

    main.go hello world

--- go/main.go
+++ go/main.go
@@ -17,7 +17,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+	fmt.Fprintf(w, "Hello world!") //这个写入到w的是输出到客户端的
 }
  
 func main() {
