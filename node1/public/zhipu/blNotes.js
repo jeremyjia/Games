bba7e10ea69f8441c317798104f36b7a1d93ecb3
littleflute <littleflute@163.com>
2022-04-30 01:19:21 +0800

    makeMusic

--- node1/public/zhipu/blNotes.js
+++ node1/public/zhipu/blNotes.js
@@ -1,35 +1,37 @@
 
-var _notes1 = function(lsNotes,x,y,dx,_makeText,_use_shuzi_by_id,_use_yingao_by_id){    
-        const _getNoteId = function(c){
-            var sID = "";
-            if(c=='1'|| c=='2'|| c=='3'|| c=='4'|| c=='5'|| c=='6'|| c=='7'){
-                sID = "shuzi_c_" + c;
-            }
-            else if(c=='|'){
-                sID = "xiaojiexian";
-            } 
-            else if(c=='-'){
-                sID = "yanyinfu";
-            }
-            else{
-                sID = "yingao_di";
-            }
-            return sID;
-        }
+    var _notes = function(lsNotes,x,y,dx,_makeText,_use_shuzi_by_id,_use_yingao_by_id){     
+        
         var s = "";
-        s += _makeText("notes: v0.13",111, 11,"yellow");
+        s += _makeText("notes: xv0.211",222, 11, 36, "yellow");
         var l = lsNotes;
 
         for(i in l){            
-            var idNote = _getNoteId(l[i][0]);
+            var idNote = blo0._getNoteId(l[i][0]);
             s += _use_shuzi_by_id(idNote,x + i*dx,y);  
           
             var nt = l[i].split(',');
- 
-
             if(nt.length>1){
                 for(var j = 0; j < (nt.length -1);j++){
                     s += _use_yingao_by_id("yingao_di",x + i*dx,y + j*8);
+                }
+            }
+
+            var fdNt = l[i].split('.');    
+            if(fdNt.length>1){
+                for(var j = 0; j < (fdNt.length -1);j++){
+                    s += _use_yingao_by_id("fudian",x + i*dx,y - j*8);
+                }
+            }
+            var jsNt = l[i].split('/');    
+            if(jsNt.length>1){
+                for(var j = 0; j < (jsNt.length -1);j++){
+                    s += _use_yingao_by_id("jianShi",x + i*dx,y - j*8); 
+                }
+            }
+            var ly = l[i].split('_');    
+            if(ly.length>1){
+                for(var j = 0; j < (ly.length -1);j++){
+                    s += _makeText(ly[j+1],x + i*dx,y + j*24 + 10, 24, "yellow");
                 }
             }
         }
