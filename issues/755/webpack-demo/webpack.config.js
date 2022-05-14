c82fe94bbf9885e2c35d649d5d2d773b1fa13aea
littleflute <littleflute@163.com>
2022-05-15 00:33:53 +0800

    data.yaml .toml .json5

--- issues/755/webpack-demo/webpack.config.js
+++ issues/755/webpack-demo/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js',
@@ -27,6 +30,27 @@ module.exports = {
       {
         test: /\.xml$/i,
         use: ['xml-loader'],
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   }, 
