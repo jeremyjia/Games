f7c499999d9002a71ecb127d353d372aaa3b563c
littleflute <littleflute@163.com>
2022-05-14 23:33:36 +0800

    webpack.config.js: module: rules

--- issues/755/webpack-demo/webpack.config.js
+++ issues/755/webpack-demo/webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  }, 
 };
