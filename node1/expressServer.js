4479473340298d53ae0403f2adbb32b43e4bcf0c
littleflute <littleflute@163.com>
2022-05-21 00:42:26 +0800

    http://localhost:3001/api/svg

--- node1/expressServer.js
+++ node1/expressServer.js
@@ -1,9 +1,14 @@
-const tag = "[expressServer.js_v0.55]"; 
+const tag = "[expressServer.js_bv0.123]"; 
 const l = require('./logger');
 const path = require('path'); 
 const express = require('express');
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
+ 
+const jsdom = require('jsdom'); 
+const { JSDOM } = jsdom;
+const d3 = import('d3');
+
 const { OpenApiValidator } = require('express-openapi-validator'); 
 const config = require('./config');
  
@@ -51,11 +56,14 @@ class ExpressServer {
     this.app.get('/downloadImage', (req, res) => {    img.download(req,res);    });
     
     this.app.get('/api/data', (req, res) => {
-      const data = [100, 50, 300, 40, 350, 250]; // assuming this is coming from the database
+      const data = [100, 50, 300, 40, 350, 250];  
       res.json(data);
     });
+    this.app.get('/api/svg', (req, res) => { 
+      s = mkSVG(res); 
+    });
 
-  }
+  } 
 
   async launch() {
     return new Promise(
@@ -71,6 +79,39 @@ class ExpressServer {
       },
     );
   }
+}
+
+
+async function mkSVG(res) {
+  const fakeDom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+
+  const outputLocation = './output.svg';
+
+  let body = (await d3).select(fakeDom.window.document).select('body');
+
+  // Make an SVG Container
+  let svgContainer = body.append('div').attr('class', 'container')
+    .append("svg")
+      .attr("width", 1280)
+      .attr("height", 1024);
+
+  // Draw a line
+  let circle = svgContainer.append("line")
+    .attr("x1", 5)
+    .attr("y1", 5)
+    .attr("x2", 500)
+    .attr("y2", 500)
+    .attr("stroke-width", 2)
+    .attr("stroke", "black");
+
+  // Output the result to console
+  console.log(body.select('.container').html());
+
+  // Output the result to file
+  
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(body.select('.container').html());
+  res.end();
 }
 
 module.exports = ExpressServer;
