628c39fbaf5dc147c2e84a566419b10c9c0ac4bb
littleflute <littleflute@163.com>
2022-06-09 00:59:07 +0800

    done

--- react/bl-react-app/src/Components/servers/index.js
+++ react/bl-react-app/src/Components/servers/index.js
@@ -10,6 +10,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import S8080 from './8080/index.js';
  
 const S3000 = () => <span>S3000</span>; 
+const S5000 = () => <span>S5000</span>; 
 
 const ServersHome = () => (
   <MemoryRouter>
@@ -20,6 +21,7 @@ const ServersHome = () => (
       <ButtonToolbar className="custom-btn-toolbar">  
           <LinkContainer to="/8080"><Button>8080</Button></LinkContainer> 
           <LinkContainer to="/3000"><Button>3000</Button></LinkContainer> 
+          <LinkContainer to="/5000"><Button>5000</Button></LinkContainer> 
       </ButtonToolbar>
       
       <h2>
@@ -27,6 +29,7 @@ const ServersHome = () => (
         <Switch> 
           <Route path="/8080">   <S8080 />    </Route> 
           <Route path="/3000">   <S3000 />    </Route> 
+          <Route path="/5000">   <S5000 />    </Route> 
         </Switch>
       </h2>
     </Container>
