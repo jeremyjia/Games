75c79a40b1787fe6fae3cbaf4aa0582a5f6d99c8
littleflute <littleflute@163.com>
2022-09-20 01:48:08 +0800

    p2.js

--- issues/814/1.py
+++ issues/814/1.py
@@ -1,3 +1,3 @@
 from music21 import converter
-s = converter.parse('1.abc')
-s.write('midi', fp='1.mid')
+s = converter.parse('2.abc')
+s.write('midi', fp='2.mid')
---
+++ issues/814/p2.js
@@ -0,0 +1,14 @@
+// in Node.js:
+var JZZ = require('jzz');
+require('jzz-midi-smf')(JZZ);
+
+var data = require('fs').readFileSync('2.mid', 'binary');
+var smf = new JZZ.MIDI.SMF(data);
+
+var player = smf.player();
+player.onEnd = function() { console.log('Done!'); };
+
+JZZ().or('Cannot start MIDI engine!').openMidiOut().or('Cannot open MIDI Out!').and(function() {
+  player.connect(this);
+  player.play();
+});
