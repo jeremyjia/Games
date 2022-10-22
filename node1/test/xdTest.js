c703dd62d33e605273ebc9bc0f707dcdf4fa0fc0
littleflute <littleflute@163.com>
2022-10-23 01:37:27 +0800

    done_abc0

--- node1/test/xdTest.js
+++ node1/test/xdTest.js
@@ -32,6 +32,6 @@ describe(tag4xdTest, function(){
   });
   
   it('word', function(){     assert.equal("done",'done');  });
-  it('zhipu', function(){     assert.equal("done",'done_zhipu');  });
+  it('abc0', function(){     assert.equal("done",'done_abc0');  });
   
 });
