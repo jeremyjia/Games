60c30f0403887dd3b1f8dcb29b18a4e85cf5b389
littleflute <littleflute@163.com>
2022-12-13 23:34:40 +0800

    xdTest.js as log

--- node1/test/xdTest.js
+++ node1/test/xdTest.js
@@ -31,10 +31,11 @@ describe(tag4xdTest, function(){
     assert.equal(app(),'to test xdApp: done.');
   });
   
-  it('word', function(){     assert.equal("done",'done');  });
-  it('abc0', function(){     assert.equal("done",'done');  });
-  it('blscript editor', function(){     assert.equal("done",'done');  });
-  it('testC4AutoRun.html', function(){     assert.equal("done",'to do: testC4AutoRun.html');  }); 
-  it('https://nodejs.dovov.com/node-js-msgwindows-api.html', function(){assert.equal("done",'2do');  });
+  it('word', function(){assert.equal("done",'done');  });
+  it('abc0', function(){assert.equal("done",'done');  });
+  it('blscript editor', function(){assert.equal("done",'done');  });
+  it('https://littleflute.github.io/Games/issues/21/testC4AutoRun.html',function(){assert.equal("done",'done');}); 
+  it('https://littleflute.github.io/Games/issues/21/testC4Canvas.html', function(){assert.equal("done",'done');});
+  it('https://nodejs.dovov.com/node-js-msgwindows-api.html', function(){assert.equal("done",'2do');  }); 
   
 });
