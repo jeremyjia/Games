675b2fcd92e918c330f379002d4ea2ada0c160e6
littleflute <littleflute@163.com>
2023-11-08 11:11:04 +0800

    draw_1_note*

--- Spring/src/main/java/com/pbz/demo/hello/util/music/MusicNote.java
+++ Spring/src/main/java/com/pbz/demo/hello/util/music/MusicNote.java
@@ -2,9 +2,10 @@ package com.pbz.demo.hello.util.music;
 import java.awt.Graphics2D;
 
 public class MusicNote {
-    public void draw_1_note(Graphics2D g,int x, int y,String note,int time, int tone) { 
+    public void draw_1_note(Graphics2D g,int left, int top,String note,int time, int tone) { 
         int dy = 50;
-        int y = 50;
+        int x = left;
+        int y = top;
         g.drawString("1 note",x,y);
         y+=dy;
         g.drawString(note,x,y);
