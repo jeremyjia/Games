405a9aae8feb979961de58949bcadd9c6cf76b32
jjia1 <jeremy.jia@sscinc.com>
2024-10-25 15:00:35 +0800

    click mouse

--- python/sample/clickMouse.py
+++ python/sample/clickMouse.py
@@ -1,11 +1,13 @@
 import time
 import pyautogui
+from datetime import datetime
 
 def click_timer(interval):
     while True:
         # 获取当前鼠标位置
         current_position = pyautogui.position()
-        print("The mouse position:", current_position)
+        current_time = datetime.now()
+        print(current_time, "The mouse pos:", current_position)
         
         # 模拟鼠标点击
         pyautogui.click(current_position)
@@ -14,4 +16,4 @@ def click_timer(interval):
         time.sleep(interval)
 
 if __name__ == "__main__":
-    click_timer(3 * 60)  # 5分钟=5*60秒
+    click_timer(3 * 60)  # 3分钟=3*60秒
