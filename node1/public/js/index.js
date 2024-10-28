67f01f3cb821aabc03d381d444adfae7841a4b4e
littleflute <littleflute@163.com>
2024-10-28 17:42:42 +0800

    bls2vTest*

--- node1/public/js/index.js
+++ node1/public/js/index.js
@@ -1,32 +1,42 @@
-
-
-bl$("bls2vTest").onclick = function(){
-    if(!this.md){
-        const C4UI = function(_md){ 
-            var vsb = null;
-            this.makeUI = function(){
-                const tb = blo0.blDiv(_md,_md.id+"tb","tb","gray");
-                const v = blo0.blDiv(_md,_md.id+"v","v","lightblue");
-                vsb = blo0.blSandBox(_md);
-                var n = 0;
-                for(i in bs){
-                    n++;
-                    var btn = blo0.blBtn(tb,tb.id+bs[i].id,bs[i].name,bs[i].color);
-                    btn.style.float = bs[i].float;
-                    btn.onclick = function(_b,_v,_i){
-                        return function(){
-                            bs[_i].click(_b,_v); 
-                        }
-                    }(btn,v,i)
+const C4UI = function(id,bs){  
+    const _md = blo0.blMD(id,"C4UI:" + id,333,50,500,400,"gray");
+    _md.makeUI = function(){
+        const tb = blo0.blDiv(_md,_md.id+"tb","tb","gray");
+        const v = blo0.blDiv(_md,_md.id+"v","v","lightblue");
+        const vsb = blo0.blSandBox(_md);
+        var n = 0;
+        for(i in bs){
+            n++;
+            var btn = blo0.blBtn(tb,tb.id+bs[i].id,bs[i].name,bs[i].color);
+            btn.style.float = bs[i].float;
+            btn.onclick = function(_b,_v,_i){
+                return function(){
+                    bs[_i].click(_b,_v,vsb); 
                 }
-            }
-            const bs = [
+            }(btn,v,i)
+        }
+    }
+    return _md;
+
+}
+
+const initTest = function(){
+    const bar = bl$("ID4TestBar");
+    const bt1 = blo0.blBtn(bar,bar.id+"bls2vTest","bls2vTest","gray");
+    const bt2 = blo0.blBtn(bar,bar.id+"mp3lrc2blsTest","mp3lrc2blsTest","gray");
+    bt2.onclick = function(){
+        alert("bt1");
+    }
+    
+    bt1.onclick = function(){
+        if(!this.md){
+            const btns = [
                 {
                     "id":1,
                     "name":"test1",
                     "color": "skyblue",
                     "float": "left",
-                    "click":function(_b,_v){
+                    "click":function(_b,_v,vsb){
                         _v.innerHTML = this.name;
                         
                         var url = "http://localhost:3001/bls2v?bls=https://littleflute.github.io/Games/Spring/script/video21.json"; 
@@ -42,7 +52,7 @@ bl$("bls2vTest").onclick = function(){
                     "name":"test2",
                     "color": "cyan",
                     "float": "left",
-                    "click":function(_b,_v){
+                    "click":function(_b,_v,vsb){
                         _v.innerHTML = this.name;
                         
                         var url = "http://localhost:3001/bls2v?bls=http://localhost:3001/bls/v1.json"; 
@@ -54,23 +64,17 @@ bl$("bls2vTest").onclick = function(){
                     }
                 },
             ];
+            this.nClick = 0;
+
+            this.md = new C4UI("id4_bls2v",btns);
+            this.md.makeUI();        
         }
-        this.nClick = 0;
 
-        this.md = blo0.blMD("id_md_bls2vTest","bls2vTest",333,50,500,400,"gray");
-        const oui = new C4UI(this.md);
-        oui.makeUI();        
+        this.nClick++;
+        this.innerHTML = "bls2vTest:" + this.nClick;
+        
+        _on_off_div(this,this.md);
     }
 
-    this.nClick++;
-    this.innerHTML = "bls2vTest:" + this.nClick;
-    
-    _on_off_div(this,this.md);
-}
-const initTest = function(){
-    const bar = bl$("ID4TestBar");
-    const bt1 = blo0.blBtn(bar,bar.id+"1","mp3lrc2blsTest",blColor[0]);
-    bt1.onclick = function(){
-        alert("bt1");
-    }
+
 }();
