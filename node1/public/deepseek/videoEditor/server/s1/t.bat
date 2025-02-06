e4cd8ffda21fdeadb3279cbd129a21bf10e3f2e8
littleflute <littleflute@163.com>
2025-02-07 05:48:23 +0800

    t

--- node1/public/deepseek/videoEditor/server/s1/t.bat
+++ node1/public/deepseek/videoEditor/server/s1/t.bat
@@ -1,17 +1,3 @@
-curl -X POST http://localhost:${PORT}/api/generate \\
-  -H "Content-Type: application/json" \\
-  -d '{
-    "frames": [
-      {"background": "操场", "objects": []},
-      {"background": "操场", "objects": []},
-      {"background": "马路边", "objects": []},
-      {"background": "马路边", "objects": []},
-      {"background": "树林", "objects": []},
-      {"background": "海边", "objects": []},
-      {"background": "马路边", "objects": []},
-      {"background": "马路边", "objects": []},
-      {"background": "树林", "objects": []}
-    ],
-    "fileName": "demo.mp4",
-    "fps": 24
-  }'
+curl -X POST http://localhost:3002/api/generate ^
+  -H "Content-Type: application/json" ^
+  -d "@t1.json"
