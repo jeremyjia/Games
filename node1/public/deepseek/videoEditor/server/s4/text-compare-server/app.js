89a20717ed317b4e10ff776103cd54f9b721630f
littleflute <littleflute@163.com>
2025-02-07 12:02:51 +0800

    3004

--- node1/public/deepseek/videoEditor/server/s4/text-compare-server/app.js
+++ node1/public/deepseek/videoEditor/server/s4/text-compare-server/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3004;
 
 // 中间件
 app.use(bodyParser.urlencoded({ extended: true }));
