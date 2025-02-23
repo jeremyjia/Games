17cb9574b4c54c0c41f3b20685d9e3c5e3207d40
littleflute <littleflute@163.com>
2025-02-24 02:10:48 +0800

    a2.bat

--- node1/public/deepseek/videoEditor/server/bat2s/20250224/a2.bat
+++ node1/public/deepseek/videoEditor/server/bat2s/20250224/a2.bat
@@ -74,11 +74,11 @@ echo       win.remove(); >> appClass.js
 echo     }); >> appClass.js
 echo   } >> appClass.js
  
-echo   static function openWindow1() { >> appClass.js
+echo   static   openWindow1() { >> appClass.js
 echo     this.createWindow('^<p^>这是第一个窗口内容^</p^>'); >> appClass.js
 echo   } >> appClass.js
  
-echo   static function openWindow2() { >> appClass.js
+echo   static   openWindow2() { >> appClass.js
 echo     this.createWindow('^<p^>这是第二个窗口内容^</p^>'); >> appClass.js
 echo   } >> appClass.js
 echo } >> appClass.js
