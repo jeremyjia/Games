74e3ed53a8faffa16767dcba3170d906c3c4f326
littleflute <littleflute@163.com>
2025-03-07 05:20:02 +0800

    c4MusicScript.js

---
+++ node1/public/js/util/c4MusicScript.js
@@ -0,0 +1,10 @@
+//c4MusicScript.js
+
+/**
+ * 设计一个音乐简谱编辑类 C4MusicScript
+ * 方法 setUI(div) 设置一个div，可在其上设置类的属性值。
+ * 属性值BPM： 可设置每分钟多少拍 
+ * 属性值 beatType： 可设置拍子类型，有 4/4， 6/8， 3/4，2/4，。。。
+ * 属性值 barsPerLine： 可设置每行几小节
+ *  方法 showInf(ctx,currentTime) 可在画布上显示currentTime所在位置是第几小节第几拍
+ */
